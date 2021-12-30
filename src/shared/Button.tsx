4c5e45489f88e706a7e68b07bf027aa05daac341
Michael Nolivos <michaelnolivos@gmail.com>
2021-12-30 00:24:35 -0500

    style: button width and classname prop

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -3,12 +3,16 @@ import { MouseEventHandler } from "react-syntax-highlighter/node_modules/@types/
 type Prop = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
+  className?: string;
 };
 
 const Button: React.FC<Prop> = (props) => {
   return (
     <button
-      className="px-8 py-2 border-2 border-black rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500"
+      className={
+        "w-64 px-8 py-2 border-2 border-black rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500 hover:bg-gradient-to-t " +
+        props.className
+      }
       onClick={props.onClick}
     >
       {props.children}
