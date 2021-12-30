66571c17b198ae7fe09c054bb530fd88c081ee1d
Michael Nolivos <michaelnolivos@gmail.com>
2021-12-30 00:26:01 -0500

    feat: add react user agent connector
    Wraps application in a context provider
    user-agent from request to UserAgent comp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,39 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
+import { UserAgentProvider } from "@quentin-sommer/react-useragent";
 
 import "tailwindcss/tailwind.css";
 
 function getLibrary(provider: any, connector: any) {
   return new Web3Provider(provider); // this will vary according to whether you use e.g. ethers or web3.js
 }
+
+const PageWrapper = (Comp: any) =>
+  class extends React.Component<{ ua: string }> {
+    /*
+     * Need to use args.ctx
+     * See https://nextjs.org/docs/advanced-features/custom-document
+     */
+    static async getInitialProps(args: any) {
+      return {
+        ua: args.ctx.req
+          ? args.ctx.req.headers["user-agent"]
+          : navigator.userAgent,
+        ...(Comp.getInitialProps ? await Comp.getInitialProps(args) : null),
+      };
+    }
+
+    render() {
+      const { ua, ...props } = this.props;
+      return (
+        <UserAgentProvider ua={ua}>
+          <Comp {...props} />
+        </UserAgentProvider>
+      );
+    }
+  };
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,4 +43,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default PageWrapper(MyApp);
