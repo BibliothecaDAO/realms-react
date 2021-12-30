8ddef3f24add1945fd1864abd48027b3d949d4d0
Michael Nolivos <michaelnolivos@gmail.com>
2021-12-29 23:13:12 -0500

    refactor: l2 wallet file and var names
    Makes differentiating with l1 var names easier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,22 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 
-import { connectWallet, isWalletConnected, walletAddress } from "../src/wallet";
+import {
+  connectWallet,
+  isWalletConnected,
+  walletAddress,
+} from "../src/l2wallet";
+import Button from "../src/shared/Button";
 
 const Home: NextPage = () => {
-  const [isConnected, setIsConnected] = useState(isWalletConnected());
+  const [isL2Connected, setIsL2Connected] = useState(isWalletConnected());
   const [l2Address, setL2Address] = useState<string>();
 
-  const handleConnectClick = async () => {
+
+  const handleL2ConnectClick = async () => {
     try {
       await connectWallet();
-      setIsConnected(isWalletConnected());
+      setIsL2Connected(isWalletConnected());
       setL2Address(await walletAddress());
     } catch (e) {
       console.error(e);
@@ -25,8 +31,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex items-center justify-center min-h-screen">
-        {isConnected ? (
+        {isL2Connected ? (
           <>
             <h3 style={{ margin: 0 }}>
               StarkNet Wallet address: <code>{l2Address}</code>
@@ -35,13 +40,7 @@ const Home: NextPage = () => {
           </>
         ) : (
           <>
-            <button
-              className="px-8 py-2 mr-4 border-2 border-black rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500 "
-              onClick={handleConnectClick}
-            >
-              Connect StarkNet Wallet
-            </button>
-            <p>First connect wallet to use dapp.</p>
+            <Button onClick={handleL2ConnectClick}>Connect to StarkNet</Button>
           </>
         )}
       </main>
--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,15 +1,19 @@
 import { MouseEventHandler } from "react-syntax-highlighter/node_modules/@types/react";
 
 type Prop = {
-    onClick: MouseEventHandler<HTMLButtonElement>;
-    children: React.ReactNode
-}
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+};
 
 const Button: React.FC<Prop> = (props) => {
-    return <button
-    className="px-8 py-2 mr-4 border-2 border-black rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500 "
-    onClick={props.onClick}
-  >{props.children}</button>
-}
+  return (
+    <button
+      className="px-8 py-2 border-2 border-black rounded-md bg-gradient-to-b from-yellow-400 to-yellow-500"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
