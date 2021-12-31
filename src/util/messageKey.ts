175279ea1ce30a71c6b97826a67831297cc00853
Michael Nolivos <michaelnolivos@gmail.com>
2021-12-30 20:26:08 -0500

    chore: signed msg formatting

--- src/util/messageKey.ts
+++ src/util/messageKey.ts
@@ -1,2 +1,2 @@
 export const messageKey = (nonce: string) =>
-  `Realms Towers. \n\nOwning any Realms, Loot, $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nNonce: ${nonce}`;
+  `Realms Tower Defence: Owning any Realms, Loot, $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nNonce: ${nonce}`;
