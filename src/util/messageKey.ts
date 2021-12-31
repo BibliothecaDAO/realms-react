5b3da05d73f7387a89868e6ded441c9e71e39084
Michael Nolivos <michaelnolivos@gmail.com>
2021-12-30 23:01:46 -0500

    chore: message key to indicate minimums
    Minimum amount of tokens

--- src/util/messageKey.ts
+++ src/util/messageKey.ts
@@ -1,2 +1,2 @@
 export const messageKey = (nonce: string) =>
-  `Realms Tower Defence: Owning any Realms, Loot, $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nNonce: ${nonce}`;
+  `Realms Tower Defence: Owning a Realms or Loot NFT, or possessing a minimum amount of $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your $ELEMENTS at this StarkNet address: ${nonce}`;
