8ca9b1c1d85edf7822501a24dfe8b60c9be9fbf0
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-04 21:42:33 -0500

    refactor: tabs in bridge
    Discard Tab from headlessui

--- src/components/bridge/Bridge.tsx
+++ src/components/bridge/Bridge.tsx
@@ -7,14 +7,20 @@ import { useStarknet } from "~/hooks/useStarknet";
 import Button from "~/shared/Button";
 import UserAgentConnector from "~/shared/UserAgentConnector";
 import { Web3Provider } from "@ethersproject/providers";
-import { Tab } from "@headlessui/react";
 import { getBagsInWallet } from "loot-sdk";
 import { messageKey } from "~/util/messageKey";
 import classNames from "classnames";
 import MintRequirements from "./MintRequirements";
 import TokenLabel from "~/shared/TokenLabel";
+import { useState } from "react";
 
 type Prop = {};
+
+type TabName =
+  | "connect-ethereum"
+  | "mint-requirements"
+  | "connect-starknet"
+  | "mint";
 
 export const Bridge: React.FC<Prop> = (props) => {
   const starknet = useStarknet();
@@ -27,6 +33,8 @@ export const Bridge: React.FC<Prop> = (props) => {
   if (error instanceof UnsupportedChainIdError) {
     console.log("TODO: Handle unsupported chain");
   }
+
+  const [currentTab, setCurrentTab] = useState<TabName>("connect-ethereum");
 
   const verifyAndMint = async () => {
     const sig = await library
@@ -48,135 +56,140 @@ export const Bridge: React.FC<Prop> = (props) => {
   return (
     <div className="w-full">
       <div className="p-2 mx-2 mt-4 bg-gray-700 border-2 border-black rounded-lg">
-        <Tab.Group>
-          <Tab.List className="bg-gray-800 rounded-md">
-            <Tab as="span">
-              {({ selected }) => (
-                <button
-                  className={classNames(
-                    tabBtnClasses.base,
-                    selected ? tabBtnClasses.active : null
-                  )}
-                >
-                  1. Connect Ethereum
-                </button>
+        <div>
+          <nav className="bg-gray-800 rounded-md">
+            <span>
+              <button
+                onClick={() => setCurrentTab("connect-ethereum")}
+                className={classNames(
+                  tabBtnClasses.base,
+                  currentTab == "connect-ethereum" ? tabBtnClasses.active : null
+                )}
+              >
+                1. Connect Ethereum
+              </button>
+            </span>
+            <span>
+              <button
+                onClick={() => setCurrentTab("mint-requirements")}
+                className={classNames(
+                  tabBtnClasses.base,
+                  currentTab == "mint-requirements"
+                    ? tabBtnClasses.active
+                    : null
+                )}
+              >
+                2. Review Polarity
+              </button>
+            </span>
+            <span>
+              <button
+                onClick={() => setCurrentTab("connect-starknet")}
+                className={classNames(
+                  tabBtnClasses.base,
+                  currentTab == "connect-starknet" ? tabBtnClasses.active : null
+                )}
+              >
+                3. Connect StarkNet
+              </button>
+            </span>
+            <button
+              onClick={() => setCurrentTab("mint")}
+              className={classNames(
+                tabBtnClasses.base,
+                currentTab == "mint" ? tabBtnClasses.active : null
               )}
-            </Tab>
-            <Tab as="span">
-              {({ selected }) => (
-                <button
-                  className={classNames(
-                    tabBtnClasses.base,
-                    selected ? tabBtnClasses.active : null
-                  )}
-                >
-                  2. Review Polarity
-                </button>
-              )}
-            </Tab>
-            <Tab as="span">
-              {({ selected }) => (
-                <button
-                  className={classNames(
-                    tabBtnClasses.base,
-                    selected ? tabBtnClasses.active : null
-                  )}
-                >
-                  3. Connect StarkNet
-                </button>
-              )}
-            </Tab>
-            <Tab as="span">
-              {({ selected }) => (
-                <button
-                  className={classNames(
-                    tabBtnClasses.base,
-                    selected ? tabBtnClasses.active : null
-                  )}
-                >
-                  4. Mint<TokenLabel>ELEMENTS</TokenLabel>
-                </button>
-              )}
-            </Tab>
-          </Tab.List>
-          <Tab.Panels className={"text-white"}>
-            <Tab.Panel className="p-4">
-              <p>
-                Let&apos;s review the items in your Ethereum wallet to determine
-                the polarity of their <TokenLabel>ELEMENTS</TokenLabel>.
-              </p>
-              {account ? (
-                <p className="mt-2 break-words">Connected as {account}</p>
-              ) : (
-                <UserAgentConnector>
-                  {(connectors) =>
-                    connectors.map((c, i) => (
-                      <Button
-                        key={c.name}
-                        onClick={() => activate(c.connector as any)}
-                        className="mt-4 mr-2 text-black"
-                      >
-                        {c.name}
-                      </Button>
-                    ))
-                  }
-                </UserAgentConnector>
-              )}
-            </Tab.Panel>
-            <Tab.Panel className="p-4">
-              <MintRequirements l1Address={account} />
-            </Tab.Panel>
-            <Tab.Panel className="gap-4 p-4">
-              <h3 className="text-xl">StarkNet Account</h3>
-              <p>
-                The Realms Tower Defence game is powered using{" "}
-                <a
-                  rel="noreferrer"
-                  className="underline"
-                  target={"_blank"}
-                  href="https://starkware.co/starknet/"
-                >
-                  StarkNet
-                </a>
-                , a rollup execution layer verified on Ethereum.
-              </p>
-              <p>
-                If you haven&apos;t already done so, please{" "}
-                <a
-                  rel="noreferrer"
-                  target="_blank"
-                  className="underline"
-                  href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb"
-                >
-                  download and install
-                </a>{" "}
-                the ArgentX extension, available now for the Google Chrome web
-                browser.
-              </p>
-              {starknet.active ? (
-                <p className="mt-2 break-words">
-                  Connected as {starknet.address}
+            >
+              4. Mint<TokenLabel>ELEMENTS</TokenLabel>
+            </button>
+          </nav>
+          <div className={"text-white"}>
+            {currentTab == "connect-ethereum" ? (
+              <div className="p-4">
+                <p>
+                  Let&apos;s review the items in your Ethereum wallet to
+                  determine the polarity of their{" "}
+                  <TokenLabel>ELEMENTS</TokenLabel>.
                 </p>
-              ) : (
-                <Button onClick={() => starknet.connect()} className="mt-4">
-                  Connect to ArgentX
-                </Button>
-              )}
-            </Tab.Panel>
-            <Tab.Panel className="p-4">
-              {starknet.active ? (
-                <>
-                  <p className="break-words">
-                    {messageKey(starknet.address as string)}
+                {account ? (
+                  <p className="mt-2 break-words">Connected as {account}</p>
+                ) : (
+                  <UserAgentConnector>
+                    {(connectors) =>
+                      connectors.map((c, i) => (
+                        <Button
+                          key={c.name}
+                          onClick={() => activate(c.connector as any)}
+                          className="mt-4 mr-2 text-black"
+                        >
+                          {c.name}
+                        </Button>
+                      ))
+                    }
+                  </UserAgentConnector>
+                )}
+              </div>
+            ) : null}
+            {currentTab == "mint-requirements" ? (
+              <div className="p-4">
+                <MintRequirements l1Address={account} />
+              </div>
+            ) : null}
+            {currentTab == "connect-starknet" ? (
+              <div className="gap-4 p-4">
+                <h3 className="text-xl">StarkNet Account</h3>
+                <p>
+                  The Realms Tower Defence game is powered using{" "}
+                  <a
+                    rel="noreferrer"
+                    className="underline"
+                    target={"_blank"}
+                    href="https://starkware.co/starknet/"
+                  >
+                    StarkNet
+                  </a>
+                  , a rollup execution layer verified on Ethereum.
+                </p>
+                <p>
+                  If you haven&apos;t already done so, please{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="underline"
+                    href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb"
+                  >
+                    download and install
+                  </a>{" "}
+                  the ArgentX extension, available now for the Google Chrome web
+                  browser.
+                </p>
+                {starknet.active ? (
+                  <p className="mt-2 break-words">
+                    Connected as {starknet.address}
                   </p>
-                  <Button className="mt-4" onClick={() => verifyAndMint()}>
-                    Sign
+                ) : (
+                  <Button onClick={() => starknet.connect()} className="mt-4">
+                    Connect to ArgentX
                   </Button>
-                </>
-              ) : null}
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+                )}
+              </div>
+            ) : null}
+            {currentTab === "mint" ? (
+              <div className="p-4">
+                {starknet.active ? (
+                  <>
+                    <p className="break-words">
+                      {messageKey(starknet.address as string)}
+                    </p>
+                    <Button className="mt-4" onClick={() => verifyAndMint()}>
+                      Sign
+                    </Button>
+                  </>
+                ) : null}
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
