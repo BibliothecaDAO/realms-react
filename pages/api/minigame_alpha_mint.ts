4f0351ca9142bb2d55fd3690d98a05216c8eed44
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-13 22:01:39 -0500

    fix: signer setup
    includes loading env vars
    use getSelectorFromName

--- pages/api/minigame_alpha_mint.ts
+++ pages/api/minigame_alpha_mint.ts
@@ -1,19 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { utils } from "ethers";
-import { Provider, ec, Signer } from "starknet";
-import { messageKey } from "../../src/util/messageKey";
-import { getStarkKey } from "starknet/dist/utils/ellipticCurve";
+import { Provider, ec, Signer, stark } from "starknet";
+import { messageKey } from "~/util/messageKey";
 import { fetchLordsBalance } from "~/util/fetchL1";
 import { MINIMUM_LORDS_REQUIRED } from "~/constants";
+const { getSelectorFromName } = stark;
 
 // https://gist.github.com/sbauch/a3405609f2fe858c4dff2ffde81c88d3
 
-const ALPHA_MINTER_ADDRESS = "";
+const ELEMENTS_ADDRESS = process.env
+  .NEXT_PUBLIC_MINIGAME_ELEMENTS_ADDRESS as string;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { sig, starknetAddress } = req.body;
 
-  // TODO: Nonce
   const ethAddress = utils.verifyMessage(messageKey(starknetAddress), sig);
 
   const lordsBalance = await fetchLordsBalance(ethAddress);
@@ -32,23 +32,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     network: "georli-alpha",
   });
 
-  const minterPrivKey = process.env.STARKNET_MINTER_MIDDLEWARE_PRIVATE_KEY;
+  const minterPrivKey = "0x" + process.env.ELEMENTS_MINTER_PRIVATE_KEY;
 
-  const privKey = ec.getKeyPair(minterPrivKey);
-  const pubKey = getStarkKey(privKey);
-  console.log("minting using account", pubKey);
-  const signer = new Signer(provider, pubKey, privKey);
-
-  const selector = "";
+  let signer;
   try {
-    const result = await signer.invokeFunction(
-      ALPHA_MINTER_ADDRESS,
+    const privKey = ec.getKeyPair(minterPrivKey);
+    const account = process.env.ELEMENTS_MINTER_ACCOUNT_ADDRESS as string;
+    signer = new Signer(provider, account, privKey);
+  } catch (e) {
+    console.error("SIGNING ERROR:", e);
+    throw e;
+  }
+
+  const selector = getSelectorFromName("mint_elements");
+  const mintArgs = [ethAddress, starknetAddress];
+  try {
+    const result = await signer?.invokeFunction(
+      ELEMENTS_ADDRESS,
       selector,
-      []
+      mintArgs
     );
     res.send(JSON.stringify(result));
     return;
   } catch (e: any) {
+    console.error("MINTING ERROR:", e);
     res.send(
       JSON.stringify({
         success: false,
