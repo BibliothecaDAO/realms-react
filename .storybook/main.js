b7f31d19ace95d922de251c5ea892307ed1f629d
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-16 22:49:30 -0500

    fix: storybook build config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
