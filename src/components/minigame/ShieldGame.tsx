10f19a41c6b83e584543c79ebeabaa2ab2740855
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-19 22:35:02 -0500

    feat: shield game component
    fetch values using StarkNet call
    Implements attack / boost shield function invocations

--- src/components/minigame/ShieldGame.tsx
+++ src/components/minigame/ShieldGame.tsx
@@ -7,49 +7,119 @@ import {
   getShieldValue,
 } from "~/util/minigameApi";
 import Button from "~/shared/Button";
+import ElementLabel from "~/shared/ElementsLabel";
+import { getStarknet } from "@argent/get-starknet/dist";
+import { defaultProvider, number, stark } from "starknet";
+import { useModuleAddress } from "~/hooks/useModuleAddress";
+import { waitForTransaction } from "~/hooks/useStarknet";
+import Castle from "./Castle";
+
+const ELEMENT_TOKEN_ADDRESS = process.env
+  .NEXT_PUBLIC_MINIGAME_ELEMENTS_ADDRESS as string;
 
 type Prop = {
   tokenId: ElementToken;
+  l2AccountAddress: string;
 };
 
 const ShieldGame: React.FC<Prop> = (props) => {
   const [gameIdx, setGameIdx] = useState<string>();
-  const [mainHealth, setMainHealth] = useState<BN | undefined>();
-  const [shieldValue, setShieldValue] = useState<BN | undefined>();
+  const [mainHealth, setMainHealth] = useState<BN>();
+  const [shieldValue, setShieldValue] = useState<
+    Record<ElementToken, BN> | undefined
+  >();
+
+  const [tokenBalance, setTokenBalance] = useState<BN>();
+
+  const towerDefenceContractAddress = useModuleAddress("1");
+
+  const fetchState = async () => {
+    const gameIdx = await getLatestGameIndex();
+
+    const gameQueries = await Promise.all([
+      getMainHealth(gameIdx),
+      getShieldValue(gameIdx, ElementToken.Light),
+      getShieldValue(gameIdx, ElementToken.Dark),
+      // get element token balance
+      defaultProvider.callContract({
+        contract_address: ELEMENT_TOKEN_ADDRESS,
+        entry_point_selector: stark.getSelectorFromName("balance_of"),
+        calldata: [props.l2AccountAddress, props.tokenId.toString()],
+      }),
+    ]);
+
+    setGameIdx(gameIdx);
+    setMainHealth(gameQueries[0]);
+    setShieldValue({
+      [ElementToken.Light]: gameQueries[1],
+      [ElementToken.Dark]: gameQueries[2],
+    });
+    setTokenBalance(number.toBN(gameQueries[3].result[0]));
+  };
 
   useEffect(() => {
-    const fetchState = async () => {
-      const gameIdx = await getLatestGameIndex();
-
-      const gameQueries = await Promise.all([
-        getMainHealth(gameIdx),
-        getShieldValue(gameIdx, props.tokenId),
-      ]);
-
-      setGameIdx(gameIdx);
-      setMainHealth(gameQueries[0]);
-      setShieldValue(gameQueries[1]);
-    };
     fetchState();
   }, []);
 
+  const handleAttack = async (amount: number) => {
+    await getStarknet().enable();
+    const res = await getStarknet().signer?.invokeFunction(
+      towerDefenceContractAddress as string,
+      stark.getSelectorFromName("attack_tower"),
+      [gameIdx as string, props.tokenId.toString(), amount.toString()]
+    );
+
+    await waitForTransaction(res?.transaction_hash as string);
+    console.log("attack transaction finished");
+    await fetchState();
+  };
+
+  const handleShield = async (amount: number) => {
+    await getStarknet().enable();
+    const res = await getStarknet().signer?.invokeFunction(
+      towerDefenceContractAddress as string,
+      stark.getSelectorFromName("increase_shield"),
+      [gameIdx as string, props.tokenId.toString(), amount.toString()]
+    );
+    await waitForTransaction(res?.transaction_hash as string);
+    console.log("shield transaction finished");
+    await fetchState();
+  };
+
+  // Stick to just single shielding/attack amount for now
+  const actionAmount = 1;
+
   return (
     <>
-      <h3>Current Game Index: {gameIdx}</h3>
-      <p>
-        You&apos;ve chosen the{" "}
-        {props.tokenId == ElementToken.Light ? "Light" : "Dark"}
+      <h3 className="text-2xl">Game # {gameIdx}</h3>
+      <p className="text-xl">
+        <ElementLabel>ELEMENTS</ElementLabel>
+        <br />
+        {props.tokenId == ElementToken.Light ? "Light" : "Dark"}{" "}
+        {tokenBalance?.toString()}
       </p>
 
-      <p>Main health: {mainHealth?.toString()}</p>
-      <p>Shield Value: {shieldValue?.toString()}</p>
-      <br />
+      <p className="text-2xl">Fortress</p>
+      <p>Vitality: {mainHealth?.toString()}</p>
       {props.tokenId == ElementToken.Light ? (
-        <Button>Shield Boost</Button>
+        <p>
+          Shield Value:{" "}
+          {shieldValue ? shieldValue[ElementToken.Dark].toString() : "-"}
+        </p>
       ) : null}
       {props.tokenId == ElementToken.Dark ? (
-        <Button>Attack Shield</Button>
+        <p>
+          Shield Value:{" "}
+          {shieldValue ? shieldValue[ElementToken.Light].toString() : "-"}
+        </p>
       ) : null}
+
+      <br />
+      <Button onClick={() => handleAttack(actionAmount)}>Boost Shield</Button>
+      <Button className="mb-4 ml-2" onClick={() => handleShield(actionAmount)}>
+        Attack Shield
+      </Button>
+      {mainHealth ? <Castle health={mainHealth.toNumber()} /> : null}
     </>
   );
 };
---
+++ src/stories/ShieldGame.stories.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ShieldGame from "~/components/minigame/ShieldGame";
+import {
+  buildStarknetUrl,
+  createStarknetNetworkMock,
+  StarknetCall,
+} from "~/mocks/starknetMockFactory";
+import { ElementToken } from "~/constants";
+import { stark } from "starknet";
+import { SelectorName } from "~/util/minigameApi";
+import { rest } from "msw";
+const { getSelectorFromName } = stark;
+
+export default {
+  title: "ShieldGame",
+  component: ShieldGame,
+} as ComponentMeta<typeof ShieldGame>;
+
+const Template: ComponentStory<typeof ShieldGame> = (args) => (
+  <ShieldGame {...args} />
+);
+
+export const Light = Template.bind({});
+Light.args = {
+  tokenId: ElementToken.Light,
+  l2AccountAddress: "0x12345678",
+};
+Light.parameters = {
+  msw: [createStarknetNetworkMock(["0x4"])],
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  tokenId: ElementToken.Dark,
+  l2AccountAddress: "0x12345678",
+};
+// Inject msw (mock service worker) with the REST handlers
+// that simulate calls to the StarkNet API based on selector name
+Dark.parameters = {
+  msw: [
+    rest.post<StarknetCall>(
+      buildStarknetUrl("alpha4.starknet.io") + "call_contract",
+      (req, res, ctx) => {
+        const responsesBySelector = {
+          [getSelectorFromName(SelectorName.getLatestGameIndex)]: "0x1",
+          [getSelectorFromName(SelectorName.getMainHealth)]: "0x10",
+          [getSelectorFromName(SelectorName.getShieldValue)]: "0x12",
+          [getSelectorFromName("get_module_address")]: "0x123",
+          [getSelectorFromName("balance_of")]: "0x123",
+        };
+
+        console.log(responsesBySelector);
+
+        if (responsesBySelector[req.body.entry_point_selector]) {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              result: [responsesBySelector[req.body.entry_point_selector]],
+            })
+          );
+        }
+        throw "Unhandled dynamic mock";
+      }
+    ),
+    rest.get(
+      buildStarknetUrl("alpha4.starknet.io") + "get_transaction_status",
+      (_req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            tx_status: "ACCEPTED_ON_L2",
+          })
+        );
+      }
+    ),
+  ],
+};
