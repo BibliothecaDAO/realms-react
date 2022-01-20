7547e73b6965217b46c3b840ee78d893a620bacf
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-19 22:33:07 -0500

    feat: castle component
    renders differently based on health ratio
    Includes stories

---
+++ src/components/minigame/Castle.tsx
@@ -0,0 +1,22 @@
+export const MAX_HEALTH = 10000;
+
+type Prop = {
+  health: number;
+};
+
+const Castle: React.FC<Prop> = (props) => {
+  let variantImg = "/assets/castle/variant1";
+  if (props.health > MAX_HEALTH / 2) {
+    variantImg = "/assets/castle/variant1";
+  } else if (props.health <= MAX_HEALTH / 2) {
+    if (props.health >= 0 && props.health < MAX_HEALTH / 4) {
+      variantImg = "/assets/castle/variant1_destroyed";
+    } else {
+      variantImg = "/assets/castle/variant1_damaged";
+    }
+  }
+
+  return <img style={{ maxWidth: "400px" }} src={variantImg + ".png"} />;
+};
+
+export default Castle;
