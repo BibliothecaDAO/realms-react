1148baeecef679dde09cccd306b6e022193c3d03
Michael Nolivos <michaelnolivos@gmail.com>
2022-01-28 14:33:23 -0500

    chore: change message key
    remove divine and NFT references, just use lords for now

--- src/util/messageKey.ts
+++ src/util/messageKey.ts
@@ -1,2 +1,2 @@
 export const messageKey = (nonce: string) =>
-  `Realms Tower Defence: Owning a Realms or Loot NFT, or possessing a minimum amount of $LORDS or $DIVINE grants you free $ELEMENTS to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your $ELEMENTS at this StarkNet address: ${nonce}`;
+  `Realms Tower Defence: Possessing a minimum amount of $LORDS grants you free ELEMENT NFTs to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your ELEMENTS at this StarkNet address: ${nonce}`;
