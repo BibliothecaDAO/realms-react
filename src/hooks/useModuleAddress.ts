7213e66b83b87b6c5052ae8a60af03dd979fb03d
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-03 18:37:17 -0500

    chore: throw on failed attempt to get module addr

--- src/hooks/useModuleAddress.ts
+++ src/hooks/useModuleAddress.ts
@@ -4,7 +4,11 @@ import { getModuleAddress } from "~/util/minigameApi";
 export const useModuleAddress = (moduleId: string) => {
   const [moduleAddress, setModuleAddress] = useState<string>();
   useEffect(() => {
-    getModuleAddress(moduleId).then((addr) => setModuleAddress(addr));
+    try {
+      getModuleAddress(moduleId).then((addr) => setModuleAddress(addr));
+    } catch (e: any) {
+      throw new Error("Couldn't retrieve module address from controller");
+    }
   }, []);
   return moduleAddress;
 };
