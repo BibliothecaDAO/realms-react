b40e0bc40f1e404ab0b13ce929a9cf5c436d3f70
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-06 00:36:33 -0500

    fix: minting API uses live module address

--- pages/api/minigame_alpha_mint.ts
+++ pages/api/minigame_alpha_mint.ts
@@ -4,10 +4,8 @@ import { Provider, ec, Signer, stark, encode } from "starknet";
 import { messageKey } from "~/util/messageKey";
 import { fetchLordsBalance } from "~/util/fetchL1";
 import { MINIMUM_LORDS_REQUIRED } from "~/constants";
+import { getModuleAddress } from "~/util/minigameApi";
 const { getSelectorFromName } = stark;
-
-const ELEMENTS_ADDRESS = process.env
-  .NEXT_PUBLIC_MINIGAME_ELEMENTS_ADDRESS as string;
 
 export type MintingError = {
   success: false;
@@ -81,9 +79,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     amount,
   ];
 
+  const elementBalancerModule = await getModuleAddress("4");
   try {
     const result = await signer?.invokeFunction(
-      ELEMENTS_ADDRESS,
+      elementBalancerModule,
       selector,
       mintArgs
     );
