2c69576b48dec3521242d46f767c481576309736
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-06 00:35:50 -0500

    chore: shield game layout on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import BridgeModal from "components/bridge/Modal";
+import ShieldGame from "~/components/minigame/ShieldGame";
 
 const Home: NextPage = () => {
-  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="">
       <Head>
@@ -15,13 +13,9 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <BridgeModal isOpen={modalOpen} toggle={() => setModalOpen(false)} />
-      <button
-        onClick={() => setModalOpen(true)}
-        className="fixed px-4 py-2 bg-gray-400 rounded-md top-2 left-2"
-      >
-        Show Modal
-      </button>
+      <div className="p-4">
+        <ShieldGame />
+      </div>
     </div>
   );
 };
