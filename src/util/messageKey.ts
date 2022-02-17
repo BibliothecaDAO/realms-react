92341b4664354e6af3e51ad898224f7510639cca
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-17 10:57:58 -0500

    fix typo

--- src/util/messageKey.ts
+++ src/util/messageKey.ts
@@ -1,2 +1,2 @@
 export const messageKey = (nonce: string) =>
-  `Deseige: Possessing a minimum amount of $LORDS grants you free ELEMENT Tokens to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your ELEMENTS at this StarkNet address:`;
+  `Desiege: Possessing a minimum amount of $LORDS grants you free ELEMENT Tokens to use in the game. Signing does not spend or burn any NFTs or ERC20 tokens.\n\nYou'll receive your ELEMENTS at this StarkNet address:`;
