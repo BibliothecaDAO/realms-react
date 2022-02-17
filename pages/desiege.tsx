b12be32e20cbe277de0903941b577e449249ffec
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-16 22:57:10 -0500

    feat: generated image for OpenGraph

--- pages/desiege.tsx
+++ pages/desiege.tsx
@@ -11,6 +11,10 @@ const Home: NextPage = () => {
           name="description"
           content="Dark elements of chaos descend on the Divine City. The Council of Mages cast an ancient spell from within the Citadel to distill Light elements in a desperate attempt to strengthen the shield and protect the city."
         />
+        <meta
+          property="og:image"
+          content="https://gen.postmage.com/generate/414f4b87-4d9c-44a8-8877-1437da67003b/f40b6de5-abe4-434f-878d-89ed4f3fcad8/image.png?url=https://lootverse.bibliothecadao.xyz/desiege"
+        />
         <link rel="icon" href="/favicon.ico" />
         {/* Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
