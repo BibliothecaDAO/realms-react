5ec082d01b63a88f635e001cfb95ec74243e4ab3
Michael Nolivos <michaelnolivos@gmail.com>
2022-02-19 16:41:35 -0500

    fix: next.config.js config failure
    Remove 2 modules
    Bump tsconfig target to ES6

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
-const withSvgr = require('next-plugin-svgr');
 const withTM = require("next-transpile-modules")([
-  "three",
-  "react-three-fiber",
-  "drei",
+  // Names of modules that need transpilation
 ]);
 
-module.exports = withPlugins([withSvgr, [withTM, {
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['d23fdhqc1jb9no.cloudfront.net']
+    domains: ["d23fdhqc1jb9no.cloudfront.net"],
   },
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: {
-        loader: "file-loader",
+  webpack(config, _options) {
+    config.module.rules.push(
+      {
+        test: /\.(glb|gltf)$/,
+        use: {
+          loader: "file-loader",
+        },
       },
-    },
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      });
+        use: ["@svgr/webpack"],
+      }
+    );
 
     return config;
   },
-}]]);
+};
+
+module.exports = withTM(nextConfig);
