115bda7bf860daad95f8d77b8f4e357c10634241
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-02-19 20:31:58 +1100

    add next image

--- src/components/realms/Realm.tsx
+++ src/components/realms/Realm.tsx
@@ -2,11 +2,10 @@ import { ReactElement } from "react";
 import React from "react";
 import { Realm } from "../../types";
 import { Resources } from "~/util/resources";
-import { TheOrders } from "~/util/theOrders";
 import { RealmProps } from "../../types";
 import { shortenAddress } from "~/util/formatters";
 import { OrderIcon } from "~/shared/OrderIcon";
-
+import Image from "next/image";
 const variantMaps: any = {
   small: { heading: "lg:text-4xl", regions: "lg:text-xl" },
 };
@@ -18,7 +17,7 @@ export function Realm(props: RealmProps): ReactElement {
   return (
     <div className="h-auto p-1 rounded-xl sm:p-4 z-10 text-white">
       {props.loading ? (
-        <div>
+        <div className="">
           <div className="w-full h-64 pt-20 mb-4 rounded bg-white/40 animate-pulse" />
           <div className="w-full h-32 pt-20 mb-4 rounded bg-white/40 animate-pulse" />
           <div className="w-full h-32 pt-20 rounded bg-white/40 animate-pulse" />
@@ -42,10 +41,11 @@ export function Realm(props: RealmProps): ReactElement {
             ""
           )}
 
-          <img
+          <Image
             src={`https://d23fdhqc1jb9no.cloudfront.net/renders_webp/${props.realm.id}.webp`}
             alt="map"
             className="w-full mt-4 rounded-xl"
+            layout="fill"
           />
           <div className="p-2">
             {props.realm.currentOwner && (
