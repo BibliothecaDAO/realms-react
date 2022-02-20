dce67ca2ce364eb4ca67dd1db854857cdbc33698
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-02-20 12:56:45 +1100

    icon tweaks

--- src/components/map/MenuSideBar.tsx
+++ src/components/map/MenuSideBar.tsx
@@ -1,10 +1,12 @@
 import { useUIContext } from "~/hooks/useUIContext";
 import { MouseEventHandler, useCallback, useState } from "react";
-import Box from "../../../public/svg/box.svg";
-import Map from "../../../public/svg/map.svg";
-import Shield from "../../../public/svg/shieldSmall.svg";
 import VolumeIcon from "../../../public/svg/volume-2.svg";
 import VolumeMuteIcon from "../../../public/svg/volume-x.svg";
+import Castle from "../../../public/svg/castle.svg";
+import Danger from "../../../public/svg/danger.svg";
+import Mountain from "../../../public/svg/mountain.svg";
+import Library from "../../../public/svg/library.svg";
+import Crown from "../../../public/svg/crown.svg";
 import { useSound } from "~/context/soundProvider";
 import { useWalletContext } from "~/hooks/useWalletContext";
 type Props = {
@@ -28,7 +30,9 @@ export const MenuSideBar = () => {
     toggleSound();
   }, [toggleSound]);
 
-  const iconClasses = "p-4 hover:bg-white/30 sm:py-8 text-xl text-off-200";
+  const buttonClasses =
+    "p-2 sm:p-4 hover:bg-white/30 sm:py-8 sm:text-xl text-off-200";
+  const iconClasses = "mx-auto w-4 sm:w-10 fill-current mb-1";
   return (
     <div
       className={`w-full sm:h-screen bottom-0 sm:w-32 sm:right-0 sm:top-0 sm:justify-center  bg-white/50  z-10 absolute backdrop-blur-md flex sm:flex-col justify-evenly transform duration-300 transition-all ${
@@ -36,26 +40,31 @@ export const MenuSideBar = () => {
       }`}
     >
       {account && (
-        <button className={iconClasses} onClick={toggleEmpireMenu}>
-          👑 <br /> My Empire
+        <button className={buttonClasses} onClick={toggleEmpireMenu}>
+          <Crown className="mx-auto w-8 sm:w-16 fill-current mb-1" />
+
+          <span className="hidden sm:block">My Empire</span>
         </button>
       )}
 
-      <button className={iconClasses} onClick={toggleMapMenu}>
-        <Map className="mx-auto" />
-        Realms
+      <button className={buttonClasses} onClick={toggleMapMenu}>
+        <Castle className={iconClasses} />
+        <span className="hidden sm:block">Realms</span>
       </button>
-      <button className={iconClasses} onClick={toggleCryptsMenu}>
-        <Map className="mx-auto" />
-        Crypts
+      <button className={buttonClasses} onClick={toggleCryptsMenu}>
+        <Danger className={iconClasses} />
+
+        <span className="hidden sm:block">Crypts</span>
       </button>
-      <button className={iconClasses} onClick={toggleResourceMenu}>
-        <Box className="mx-auto" />
-        Resources
+      <button className={buttonClasses} onClick={toggleResourceMenu}>
+        <Mountain className={iconClasses} />
+
+        <span className="hidden sm:block">Resources</span>
       </button>
-      <button className={iconClasses} onClick={toggleTheOrdersMenu}>
-        <Shield className="mx-auto" />
-        Orders
+      <button className={buttonClasses} onClick={toggleTheOrdersMenu}>
+        <Library className={iconClasses} />
+
+        <span className="hidden sm:block">Orders</span>
       </button>
       {/* <button
         className="p-4 py-8 text-xl hover:bg-white/30 text-off-200"
