228c22b790cdc849d982baba0d89ea1543877343
Michael Nolivos <michaelnolivos@gmail.com>
2022-03-03 18:15:51 -0500

    fix(desiege): inline svgs in storybook with svgr
    From https://medium.com/@derek_19900/config-storybook-4-to-use-svgr-webpack-plugin-22cb1152f004

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 var path = require("path");
 const webpack = require("webpack");
 
+const pathToInlineSvg = path.resolve(__dirname, "../public/svg");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
@@ -32,6 +34,29 @@ module.exports = {
       ...(config.resolve.modules || []),
       path.resolve(__dirname, "../src"),
     ];
+
+    const rules = config.module.rules;
+
+    // modify storybook's file-loader rule to avoid conflicts with svgr
+    const fileLoaderRule = rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = pathToInlineSvg;
+
+    rules.push({
+      test: /\.svg$/,
+      include: pathToInlineSvg,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    config.module.rules = [...rules];
 
     config.resolve.fallback = {
       ...config.resolve.fallback,
