bd1d8553cb6d1b22b7dbad527981141495ae38f9
RedBeard <redbeard.eth@protonmail.com>
2022-03-14 17:20:20 +1100

    fix: dedupe yarn and update tests

--- apps/atlas/src/util/minigameApi.test.tsx
+++ apps/atlas/src/util/minigameApi.test.tsx
@@ -6,17 +6,17 @@ import {
 
 describe('Dynamic element balancing', () => {
   test('calculates next mint amount', () => {
-    let light = 100;
-    let dark = 400;
-    let next = getNextMintAmount({ light, dark });
+    const light = 10000;
+    const dark = 40000;
+    const next = getNextMintAmount({ light, dark });
     expect(next).toBe((dark - light) / 2);
   });
   test('uses minimum amount', () => {
-    let multiplier = EFFECT_BASE_FACTOR; // to convert to basis points
-    let light = MINIMUM_MINT_AMOUNT * multiplier;
-    let dark = MINIMUM_MINT_AMOUNT * 2 * multiplier;
+    const multiplier = EFFECT_BASE_FACTOR; // to convert to basis points
+    const light = MINIMUM_MINT_AMOUNT * multiplier;
+    const dark = MINIMUM_MINT_AMOUNT * 2 * multiplier;
     // diff would be MIN / 2 which is lower than minimum
-    let next = getNextMintAmount({ light, dark });
+    const next = getNextMintAmount({ light, dark });
     expect(next).toEqual(MINIMUM_MINT_AMOUNT * multiplier);
   });
 });
