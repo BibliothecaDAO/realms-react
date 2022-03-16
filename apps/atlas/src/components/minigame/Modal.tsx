8f11965d51a5b4c076ccfb7e914f82bb81f2a2c0
Michael Nolivos <michaelnolivos@gmail.com>
2022-03-16 15:55:09 -0400

    fix(desiege): modal overlap during mint

--- apps/atlas/src/components/minigame/Modal.tsx
+++ apps/atlas/src/components/minigame/Modal.tsx
@@ -14,7 +14,7 @@ const Modal: React.FC<Prop> = (props) => {
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 z-20 overflow-y-auto "
+        className="fixed inset-0 z-10 overflow-y-auto "
         open={isOpen}
         onClose={() => props.toggle()}
       >
