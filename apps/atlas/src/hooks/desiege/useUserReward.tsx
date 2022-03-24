2a35d8cc8e4d4ac9554ed24bf16698c1f7a2a482
Michael Nolivos <michaelnolivos@gmail.com>
2022-03-23 22:13:52 -0400

    feat(desiege): useUserReward game winning side
    fetches and computes game winning side from the contract

--- apps/atlas/src/hooks/desiege/useUserReward.tsx
+++ apps/atlas/src/hooks/desiege/useUserReward.tsx
@@ -1,6 +1,7 @@
 import { useContract, useStarknetCall } from '@starknet-react/core';
 import type BN from 'bn.js';
 import type { Abi } from 'starknet';
+import { toBN } from 'starknet/dist/utils/number';
 import TowerDefenceStorageContract from '@/abi/minigame/02_TowerDefenceStorage.json';
 
 import { useModuleAddress } from '../useModuleAddress';
@@ -8,11 +9,9 @@ import { useModuleAddress } from '../useModuleAddress';
 const useUserReward = ({
   gameIdx,
   account,
-  side,
 }: {
   gameIdx?: number;
   account?: string;
-  side: string;
 }) => {
   const towerDefenceStorageAddr = useModuleAddress('2');
 
@@ -21,12 +20,31 @@ const useUserReward = ({
     abi: TowerDefenceStorageContract as Abi,
   });
 
+  const getMainHealth = useStarknetCall({
+    contract,
+    method: 'get_main_health',
+    args: gameIdx !== undefined ? [gameIdx] : undefined,
+  });
+
+  const gameFinalHealth: BN | undefined = getMainHealth.data
+    ? getMainHealth.data[0]
+    : undefined;
+  let gameWinningSide: string | undefined;
+  if (gameFinalHealth && gameFinalHealth.toNumber() > 0) {
+    gameWinningSide = 'light';
+  } else {
+    gameWinningSide = 'dark';
+  }
+
   const getUserReward = useStarknetCall({
     contract,
     method: 'get_user_reward_alloc',
     args:
-      gameIdx && towerDefenceStorageAddr
-        ? [gameIdx, account, side == 'light' ? 0 : 1]
+      gameIdx !== undefined &&
+      account &&
+      towerDefenceStorageAddr &&
+      gameWinningSide
+        ? [gameIdx, account, gameWinningSide == 'light' ? 0 : 1]
         : undefined,
   });
 
@@ -35,8 +53,7 @@ const useUserReward = ({
     : undefined;
 
   return {
-    loading: getUserReward.loading,
-    refresh: getUserReward.refresh,
+    loading: getUserReward.loading || getMainHealth.loading,
     alloc,
   };
 };
