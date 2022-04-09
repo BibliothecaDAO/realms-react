28da5df3f9ab744bf5198f078fdf5977a1a1e1cd
Michael Nolivos <michaelnolivos@gmail.com>
2022-04-08 20:37:40 -0400

    feat(desiege): show staked realm requirement
    shows amount of staked Realm NFTs bridge
    show state of requirement satisfaction via check icon

--- apps/atlas/src/shared/Icons.tsx
+++ apps/atlas/src/shared/Icons.tsx
@@ -3,9 +3,11 @@ import {
   DocumentTextIcon,
   VolumeUpIcon,
   VolumeOffIcon,
+  CheckCircleIcon,
 } from '@heroicons/react/solid';
 
 export const ExternalLink = ExternalLinkIcon;
 export const DocumentText = DocumentTextIcon;
 export const VolumeUp = VolumeUpIcon;
 export const VolumeOff = VolumeOffIcon;
+export const CheckCircle = CheckCircleIcon;
