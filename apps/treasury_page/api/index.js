32e3d91154fc72b8fe7a0c097da9ad84334cf27d
kalaiy21 <kalaiytz2132002@gmail.com>
2022-04-19 21:16:36 +0530

    resolved the issue

--- apps/treasury_page/api/index.js
+++ apps/treasury_page/api/index.js
@@ -1,125 +1,127 @@
+
+
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-const { tableDestruct, TotalAssest } = require('./tableDestructure');
-const bodyParser = require('body-parser');
+const {tableDestruct,TotalAssest} = require('./tableDestructure');
+const  bodyParser = require('body-parser');
 
-const {
-  destruct,
-  destructList,
-  nftitems,
-  FilterByClickList,
-} = require('./nftsdestruct');
-const Moralis = require('moralis/node');
+const {destruct,destructList,nftitems,FilterByClickList}  = require('./nftsdestruct');
+const {lords} = require('./lords');
+const Moralis  = require('moralis/node');
 const app = express();
+
 
 app.use(bodyParser.json());
 
-const serverUrl = 'https://sawrxn3is56r.usemoralis.com:2053/server';
-const appId = 'OyMwhQ8JhFzPgsKc8QZ1aPQWxZhtx3Bft87gqHG5';
+const serverUrl = process.env.SERVERURL; //moralise serverUrl need to put as env
+const appId =process.env.APPID; ////moralise appId need to put as env
 Moralis.start({ serverUrl, appId });
 
-const walletaddress = '0xef3155450baa054ffe7950509ce2042613ee6586';
 
-app.get('/api/nftsapi', async function (req, res) {
+const walletaddress = process.env.WALLETADDRESS  // need to put as env
+const apikey = process.env.APIKEY
+
+app.get('/nftsapi',async function (req,res) {
+
+
   const options = { chain: 'ETH', address: walletaddress };
   const data = await Moralis.Web3API.account.getNFTs(options);
-  const nftitem = nftitems(data);
+  const nftitem = nftitems(data)
 
-  const url = `https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM:${walletaddress}`;
+    const url = `https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM:${walletaddress}`
 
-  axios
-    .get(url)
-    .then(function (response) {
-      const data = response.data;
-      res.send(destruct(nftitem, data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+    axios.get(url)
+      .then(function (response) {
+        const data = response.data
+        res.send(destruct(nftitem,data))
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+})
 
-app.get('/api/nftslistapi', async function (req, res) {
+app.get('/nftslistapi',async function (req,res) {
+
   const options = { chain: 'ETH', address: walletaddress };
   const data = await Moralis.Web3API.account.getNFTs(options);
-  res.send(destructList(data));
-});
+  res.send(destructList(data))
+  
+})
 
-app.post('/api/listclicked', function (req, res) {
-  let listclicked = req.body.list;
-  let filteredList = FilterByClickList(listclicked);
-  res.send(filteredList);
-});
+app.post('/listclicked',function (req,res) {
 
-app.get('/api/tableapi', async function (req, res) {
-  const apikey = 'EK-pYffx-aL5xsQC-o7WsN'; // need to put as env
-  const url = `https://api.ethplorer.io/getAddressInfo/${walletaddress}?apiKey=${apikey}`;
+  let listclicked = req.body.list
+  let filteredList = FilterByClickList(listclicked)
+  res.send(filteredList)
+  
+})
 
-  // this will add temp lords data to table if not in the wallet
-  let lords = {};
-  axios
-    .get(
-      'https://api.ethplorer.io/getTokenInfo/0x686f2404e77Ab0d9070a46cdfb0B7feCDD2318b0?apiKey=EK-pYffx-aL5xsQC-o7WsN'
-    )
+ 
+app.get('/tableapi',async function (req,res) {
+
+
+
+    const url = `https://api.ethplorer.io/getAddressInfo/${walletaddress}?apiKey=${apikey}`
+
+    
+    // this will add temp lords data to table if not in the wallet
+    const lordsdata = await lords()
+
+    function chklord(result) {
+      result.forEach(element => {
+        let name = element.name
+        if (name === "LORDS"){
+          return true
+        }
+      });
+      return false
+    }
+    //------------------
+    axios.get(url)
+      .then(function (response) {
+        const data = response.data
+        let result = tableDestruct(data)
+        chklord(result) ? null : result.unshift(lordsdata)
+        res.send(result)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+})
+
+app.get('/totalassest',async function (req,res) {
+
+
+  const url = `https://api.ethplorer.io/getAddressInfo/${walletaddress}?apiKey=${apikey}`
+
+  const lordsdata = await lords()
+  let lordsprice = lordsdata.inUsd
+  lordsprice = lordsprice.slice(1,-3)
+  lordsprice= lordsprice.replace(',','')
+  lordsprice= lordsprice.replace(',','')
+  lordsprice = parseInt(lordsprice)
+  //console.log(lordsprice);
+
+
+  axios.get(url)
     .then(function (response) {
-      const data = response.data;
+      const data = response.data
+      result = tableDestruct(data)
+      let total = TotalAssest(result)
+      total = total[0]+lordsprice
       var formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
       });
-      price = data.price.rate * 112500500;
-      price = formatter.format(price.toFixed(2));
-      lords = {
-        name: 'LORDS',
-        balance: '112,500,500.00',
-        inUsd: price,
-        percent: '0.00',
-      };
-    })
-
-    .catch(function (error) {
-      console.log(error);
-    });
-
-  function chklord(result) {
-    result.forEach((element) => {
-      let name = element.name;
-      if (name === 'LORDS') {
-        return true;
-      }
-    });
-    return false;
-  }
-  //------------------
-
-  axios
-    .get(url)
-    .then(function (response) {
-      const data = response.data;
-      let result = tableDestruct(data);
-      chklord(result) ? null : result.unshift(lords);
-      res.send(result);
+      total = formatter.format(total)
+      res.send([total])
     })
     .catch(function (error) {
       console.log(error);
-    });
-});
-
-app.get('/api/totalassest', async function (req, res) {
-  const apikey = 'EK-pYffx-aL5xsQC-o7WsN'; // need to put as env
-  const url = `https://api.ethplorer.io/getAddressInfo/${walletaddress}?apiKey=${apikey}`;
-
-  axios
-    .get(url)
-    .then(function (response) {
-      const data = response.data;
-      result = tableDestruct(data);
-      res.send(TotalAssest(result));
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+})
 
-app.listen(5000, function () {
-  console.log('server started in port 5000');
-});
+app.listen(5000,function () {
+    console.log("server started in port 5000");
+})
