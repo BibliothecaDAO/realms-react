77728c9312e412f880e4655296924712d6f984ef
Michael Nolivos <michaelnolivos@gmail.com>
2022-04-22 23:56:44 -0400

    feat: ably realtime sdk
    add ably package
    setup API request using env secret
    useAbly hook requests token and handles sub  channel callback

---
+++ apps/atlas/src/pages/api/createAblyTokenRequest.ts
@@ -0,0 +1,18 @@
+import Ably from 'ably/promises';
+
+// This request is sent to the server to create a new token by the useAbly.ts hook.
+// This allows WebSocket functionality to be authenticated and used in a Vercel (serverless) environment.
+export default async function handler(_req, res) {
+  const ablyApiKey = process.env.ABLY_API_KEY;
+
+  if (ablyApiKey === undefined) {
+    res.status(500).send('ABLY_API_KEY environment variable is not set');
+    return;
+  }
+
+  const client = new Ably.Realtime(ablyApiKey);
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: 'ably-nextjs',
+  });
+  res.status(200).json(tokenRequestData);
+}
