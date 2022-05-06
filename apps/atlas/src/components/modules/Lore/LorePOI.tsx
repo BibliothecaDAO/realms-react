94cc0bbc26cc2eaaa3e26d14efdcf937a709b4b0
rvorias <raphaelvorias@gmail.com>
2022-05-07 00:24:10 +0200

    fix(lore): add default poiId and fix openSideBar

--- apps/atlas/src/components/modules/Lore/LorePOI.tsx
+++ apps/atlas/src/components/modules/Lore/LorePOI.tsx
@@ -2,13 +2,18 @@ import type { LorePoiFragmentFragment } from '@/generated/graphql';
 import { useUIContext } from '@/hooks/useUIContext';
 
 const pois = {
+  0: { name: 'Unlinked', class: '' },
   1: { name: 'Scrolls', class: 'bg-red-500' },
-  1000: { name: 'Realms', class: 'bg-indigo-500' },
+  1000: { name: 'Realms', class: 'bg-indigo-500', openDetailsName: 'realm' },
   1001: { name: 'Realms Orders', class: 'bg-indigo-500' },
   1002: { name: 'Realms Resources', class: 'bg-indigo-500' },
   1003: { name: 'Realms Wonders', class: 'bg-indigo-500' },
   1004: { name: 'Realms Resources Exchange (AMM)', class: 'bg-indigo-500' },
-  2000: { name: 'Crypts and Caverns', class: 'bg-orange-500' },
+  2000: {
+    name: 'Crypts and Caverns',
+    class: 'bg-orange-500',
+    openDetailsName: 'crypt',
+  },
   3000: { name: 'Lords', class: 'bg-sky-500' },
 };
 
@@ -27,9 +32,15 @@ export const LorePOI = ({
 }: LorePOIProps) => {
   const { openDetails } = useUIContext();
 
+  // if poiId does not exist, set it to the default case
+  if (!pois[poiId]) {
+    poiId = '0';
+  }
+
   const openSideBar = () => {
-    if (assetId) {
-      openDetails(poiId === '1000' ? 'realm' : 'crypt', assetId);
+    const openDetailsName = pois[poiId].openDetailsName;
+    if (assetId && openDetailsName) {
+      openDetails(openDetailsName, assetId);
     }
   };
 
