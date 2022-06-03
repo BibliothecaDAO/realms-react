19796d16f5b8f46cdf3e36825f77c9da5af253ee
Michael Nolivos <michaelnolivos@gmail.com>
2022-06-03 11:03:25 -0400

    feat: nav header w/ network connect popover
    small button text
    removed button surrounding lords display
    fixed z-index of sidebar conflicting with popover

--- apps/atlas/src/components/navigation/header.tsx
+++ apps/atlas/src/components/navigation/header.tsx
@@ -2,23 +2,16 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { Button, IconButton } from '@bibliotheca-dao/ui-lib';
 import Crown from '@bibliotheca-dao/ui-lib/icons/crown-color.svg';
-import Ethereum from '@bibliotheca-dao/ui-lib/icons/eth.svg';
 import Lords from '@bibliotheca-dao/ui-lib/icons/lords-icon.svg';
-import StarkNet from '@bibliotheca-dao/ui-lib/icons/starknet-logo.svg';
 import VolumeOff from '@bibliotheca-dao/ui-lib/icons/volume-mute-solid.svg';
 import VolumeOn from '@bibliotheca-dao/ui-lib/icons/volume-up-solid.svg';
-import { Menu } from '@headlessui/react';
-import { useStarknet } from '@starknet-react/core';
 import { useState } from 'react';
 import useSound from 'use-sound';
 import { useUIContext } from '@/hooks/useUIContext';
-import NetworkConnect from '@/shared/NetworkConnectDropdown';
-import { shortenAddress } from '@/util/formatters';
+import NetworkConnectButton from '@/shared/NetworkConnectButton';
 import { useWalletContext } from '../../hooks/useWalletContext';
 export function Header() {
-  const { connectWallet, isConnected, disconnectWallet, displayName, balance } =
-    useWalletContext();
-  const { account, connect, connectors } = useStarknet();
+  const { balance } = useWalletContext();
   const { togglePanelType } = useUIContext();
   const [soundOn, setSoundOn] = useState(false);
   const [play, { stop }] = useSound(
@@ -56,32 +49,16 @@ export function Header() {
           />
         </div>
 
-        <span>
-          <Button variant="primary" onClick={connectWallet}>
-            <Lords className="w-6" /> <span className="px-4">{balance}</span>
-          </Button>
+        <span className="flex items-center">
+          <Lords className="w-6" /> <span className="px-4">{balance}</span>
         </span>
-        <span>
-          <NetworkConnect />
-          {/* <Button
-            variant="primary"
-            onClick={() => togglePanelType('account')}
-            className="py-1"
-          >
-            <Crown className="w-8 mr-4" />
-            <Ethereum
-              className={`w-4 mx-4 ${!isConnected ? 'filter grayscale' : ''}`}
-            />
-            <StarkNet
-              className={`w-5 mr-2 ${!account ? 'filter grayscale' : ''}`}
-            />
-          </Button> */}
-        </span>
+        <NetworkConnectButton />
         <Button
           variant="primary"
           onClick={() => togglePanelType('account')}
-          className="py-1"
+          className="py-1 text-sm"
         >
+          <Crown className="inline-block w-6 mr-2 -ml-2" />
           Account
         </Button>
       </div>
