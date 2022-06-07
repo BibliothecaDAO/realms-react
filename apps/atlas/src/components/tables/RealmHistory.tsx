a05bf2ae181cbb53d76eec2c9e6148c1421ec605
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-06-07 16:04:06 +1000

    feat: realm history

--- apps/atlas/src/components/tables/RealmHistory.tsx
+++ apps/atlas/src/components/tables/RealmHistory.tsx
@@ -1,4 +1,10 @@
-import { Table, Button, ResourceIcon } from '@bibliotheca-dao/ui-lib';
+import {
+  Table,
+  Button,
+  ResourceIcon,
+  Card,
+  CardBody,
+} from '@bibliotheca-dao/ui-lib';
 import type { ReactElement } from 'react';
 import { useGetRealmHistoryQuery } from '@/generated/graphql';
 import { shortenAddress } from '@/util/formatters';
@@ -14,119 +20,146 @@ export function RealmHistory({ realmId }: RealmHistoryProps): ReactElement {
     // { Header: 'Outcome', id: 3, accessor: 'outcome' },
   ];
 
-  const { data: historyData } = useGetRealmHistoryQuery({
+  const { data: historyData, loading: loadingData } = useGetRealmHistoryQuery({
     variables: { filter: { realmId: { equals: realmId } } },
+    pollInterval: 5000,
   });
+  const successClass = 'bg-green-200/20';
+  const negativeClass = 'bg-red-200/20';
 
   function genRealmEvent(event) {
     switch (event.eventType) {
       case 'realm_combat_attack':
-        return event.data?.success ? (
-          <span className="">
-            💰 Raid Successful <br></br> on {event.data?.defendRealmId}
-          </span>
-        ) : (
-          `😞 Unsuccessful Raid`
-        );
+        return {
+          event: event.data?.success ? (
+            <span className="">
+              💰 Raid successful on Realm {event.data?.defendRealmId}
+            </span>
+          ) : (
+            `😞 Unsuccessful Raid`
+          ),
+          class: event.data?.success ? successClass : negativeClass,
+          action: event.data?.success ? (
+            <Button
+              size="xs"
+              variant="primary"
+              href={'/ream/' + event.data?.defendRealmId}
+            >
+              Pillage and plunder again
+            </Button>
+          ) : (
+            <Button
+              size="xs"
+              variant="primary"
+              href={'/ream/' + event.data?.defendRealmId}
+            >
+              Try again
+            </Button>
+          ),
+        };
       case 'realm_combat_defend':
-        return event.data?.success ? (
-          <span className="">
-            💪 Defended Raid from {event.data?.defendRealmId}
-          </span>
-        ) : (
-          <span className="">
-            🔥 We have been Pillaged! {event.data?.defendRealmId}
-          </span>
-        );
+        return {
+          event: event.data?.success ? (
+            <span className="">
+              💪 Defended raid from {event.data?.defendRealmId}
+            </span>
+          ) : (
+            <span className="">
+              🔥 We have been Pillaged by Realm {event.data?.attackRealmId}
+            </span>
+          ),
+          class: event.data?.success ? successClass : negativeClass,
+          action: event.data?.success ? (
+            <Button
+              size="xs"
+              variant="primary"
+              href={'/ream/' + event.data?.attackRealmId}
+            >
+              Try again
+            </Button>
+          ) : (
+            <Button
+              size="xs"
+              variant="primary"
+              href={'/ream/' + event.data?.attackRealmId}
+            >
+              ⚔️ muster the troops! to battle!!
+            </Button>
+          ),
+        };
       case 'realm_building_built':
-        return `🏗️ Built ${event.data?.buildingName}`;
+        return {
+          event: `🏗️ Built ${event.data?.buildingName}`,
+          class: successClass,
+          action: '',
+        };
       case 'realm_resource_upgraded':
-        return `🏗️ Upgraded ${event.data?.resourceName} to Level ${event.data?.level}`;
+        return {
+          event: `🏗️ Upgraded ${event.data?.resourceName} to Level ${event.data?.level}`,
+          class: successClass,
+          action: '',
+        };
       case 'realm_mint':
-        return `Minted`;
+        return {
+          event: `🏗️ Minted Realm ${event.realmId}`,
+          class: successClass,
+          action: (
+            <Button size="xs" variant="primary" href={'/ream/' + event.realmId}>
+              Manage Realm
+            </Button>
+          ),
+        };
       case 'realm_settle':
-        return '🏘️ Settled';
+        return {
+          event: '🏘️ Settled',
+          class: successClass,
+          action: '',
+        };
       case 'realm_unsettle':
-        return '🏚️ Unsettled';
+        return {
+          event: '🏚️ Unsettled',
+          class: successClass,
+          action: '',
+        };
       default:
-        return '';
-    }
-  }
-
-  function genRealmAction(event) {
-    switch (event.eventType) {
-      case 'realm_combat_attack':
-        return event.data?.success ? (
-          <Button
-            size="xs"
-            variant="primary"
-            href={'/ream/' + event.data?.defendRealmId}
-          >
-            Pillage again
-          </Button>
-        ) : (
-          <Button
-            size="xs"
-            variant="primary"
-            href={'/ream/' + event.data?.defendRealmId}
-          >
-            Try again
-          </Button>
-        );
-      case 'realm_combat_defend':
-        return event.data?.success ? (
-          <Button
-            size="xs"
-            variant="primary"
-            href={'/ream/' + event.data?.attackRealmId}
-          >
-            Try again
-          </Button>
-        ) : (
-          <Button
-            size="xs"
-            variant="primary"
-            href={'/ream/' + event.data?.attackRealmId}
-          >
-            ⚔️ Retaliate
-          </Button>
-        );
-      case 'realm_building_built':
-        return `Built ${event.data?.buildingName}`;
-      case 'realm_resource_upgraded':
-        return `Upgraded ${event.data?.resourceName} to Level ${event.data?.level}`;
-      case 'realm_mint':
-        return 'Ser, welcome';
-      case 'realm_settle':
-        return 'Ser, welcome';
-      case 'realm_unsettle':
-        return 'Unsettled';
-      default:
-        return '';
+        return {
+          event: '',
+          class: '',
+          action: '',
+        };
     }
   }
 
   const realmEventData = (historyData?.getRealmHistory ?? [])
     .map((realmEvent) => {
-      console.log(realmEvent);
+      console.log(historyData?.getRealmHistory);
       return {
         event: genRealmEvent(realmEvent),
-        action: genRealmAction(realmEvent),
-        // lord: shortenAddress(realmEvent.realmOwner ?? ''),
-        // outcome: (
-        //   <div>
-        //     <p>-2 Catapults</p>
-        //     <p>+63 Coal, +8 Dragonhide</p>
-        //   </div>
-        // ),
+        timestamp: realmEvent.timestamp,
       };
     })
-    .filter((row) => row.event !== '');
+    .filter((row) => row.event.event !== '');
 
   const tableOptions = { is_striped: true, search: false };
   return (
-    <div className="flex w-full p-2">
-      <Table columns={columns} data={realmEventData} options={tableOptions} />
+    <div className="flex flex-wrap w-full p-2 space-y-2">
+      {realmEventData.map((a, index) => {
+        return (
+          <Card
+            key={index}
+            className={`w-full ${loadingData ?? 'animate-pulse'}`}
+          >
+            <CardBody className={`flex ${a.event.class} `}>
+              <span className="py-1 mb-1 text-xs font-semibold text-white">
+                {new Date(a.timestamp).toLocaleTimeString('en-US')}{' '}
+                {new Date(a.timestamp).toLocaleDateString('en-US')}
+              </span>
+              <h5 className="text-white">{a.event.event}</h5>
+              <div className="mt-4">{a.event.action}</div>
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
   );
 }
