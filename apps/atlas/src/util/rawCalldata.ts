4a4558a769b275b81c4c248d7d6c82609fe09cee
Michael Nolivos <michaelnolivos@gmail.com>
2022-07-02 10:17:11 -0400

    fix: bug in uint256 order

--- apps/atlas/src/util/rawCalldata.ts
+++ apps/atlas/src/util/rawCalldata.ts
@@ -1,5 +1,5 @@
 import type { uint256 } from 'starknet';
 
 export function uint256ToRawCalldata(num: uint256.Uint256) {
-  return [num.high, num.low];
+  return [num.low.toString(10), num.high.toString(10)];
 }
