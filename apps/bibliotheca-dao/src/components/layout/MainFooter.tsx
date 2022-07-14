e893a60b2bba7056e39d0c1aa93fc2842db09aa3
RedBeard <redbeard.eth@protonmail.com>
2022-07-14 16:46:10 +1000

    feat: update footer

--- apps/bibliotheca-dao/src/components/layout/MainFooter.tsx
+++ apps/bibliotheca-dao/src/components/layout/MainFooter.tsx
@@ -5,7 +5,7 @@ const footerLinkStyles = 'hover:underline transition-all duration-300';
 
 export const MainFooter = () => {
   return (
-    <div className="container grid grid-cols-2 gap-2 p-10 mx-auto mt-10 border-t border-white sm:grid-cols-2 sm:p-10">
+    <div className="grid w-full grid-cols-1 gap-2 p-10 mx-auto mt-10 border-t border-white sm:grid-cols-2 lg:grid-cols-4 sm:p-10">
       <div className="">
         <Image
           className="mb-10 rounded"
@@ -15,19 +15,44 @@ export const MainFooter = () => {
           height={200}
         />
       </div>
-      <div className="">
-        <h4 className="mb-6">Contact</h4>
+      <div className="mb-10">
+        <h4 className="mb-6">Links</h4>
         <ul>
           <li className={footerLinkStyles}>
-            {' '}
-            <a href={links[0].website}>Bibliotheca DAO</a>
+            <a href={links[0].website}>The Atlas</a>
           </li>
           <li className={footerLinkStyles}>
-            {' '}
-            <a href={links[0].twitter}>Twitter</a>
+            <a href={links[0].treasury}>The Treasury</a>
           </li>
           <li className={footerLinkStyles}>
-            {' '}
+            <a href={links[0].snapshot}>Snapshot Voting</a>
+          </li>
+          <li className={footerLinkStyles}>
+            <a href={links[0].scroll}>Master Scroll</a>
+          </li>
+          <li className={footerLinkStyles}>
+            <a href={links[0].staking}>Staking</a>
+          </li>
+          {/* {projects.map((a, index) => {
+            return (
+              <li className={footerLinkStyles} key={index}>
+                {' '}
+                <a href={a.website}>{a.name}</a>{' '}
+              </li>
+            );
+          })} */}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h4 className="mb-6">Socials</h4>
+        <ul>
+          <li className={footerLinkStyles}>
+            <a href={links[0].twitterBiblio}>Bibliotheca Twitter</a>
+          </li>
+          <li className={footerLinkStyles}>
+            <a href={links[0].twitterRealms}>Realms Twitter</a>
+          </li>
+          <li className={footerLinkStyles}>
             <a href={links[0].discord}>Discord</a>
           </li>
           {/* {projects.map((a, index) => {
@@ -40,6 +65,16 @@ export const MainFooter = () => {
           })} */}
         </ul>
       </div>
+      <iframe
+        src="https://discord.com/widget?id=884211910222970891&theme=dark"
+        width="350"
+        className="max-w-full"
+        title="discord-widget"
+        height="300"
+        allowtransparency="true"
+        frameBorder="0"
+        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+      ></iframe>
     </div>
   );
 };
--- apps/bibliotheca-dao/src/data/Projects.ts
+++ apps/bibliotheca-dao/src/data/Projects.ts
@@ -2,7 +2,12 @@ export const links = [
   {
     name: 'Bibliotheca DAO',
     discord: 'https://discord.gg/uQnjZhZPfu',
-    twitter: 'https://twitter.com/LootRealms',
+    twitterRealms: 'https://twitter.com/LootRealms',
+    twitterBiblio: 'https://twitter.com/BibliothecaDAO',
     website: 'https://atlas.bibliothecadao.xyz',
+    snapshot: 'https://snapshot.org/#/council.bibliotheca.eth',
+    scroll: 'https://scroll.bibliothecadao.xyz/',
+    staking: 'https://staking.bibliothecadao.xyz/',
+    treasury: '/treasury',
   },
 ];
