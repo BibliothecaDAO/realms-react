d74fd697db346ae2a2dec4b390b84134c4bfbfdc
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-07-22 22:39:02 +1000

    feat: add more logos

--- apps/bibliotheca-dao/src/components/PartnerBanner.tsx
+++ apps/bibliotheca-dao/src/components/PartnerBanner.tsx
@@ -1,5 +1,7 @@
 import Cartridge from '@bibliotheca-dao/ui-lib/icons/cartridge.svg';
 import Starkware from '@bibliotheca-dao/ui-lib/icons/starkware.svg';
+import Topology from '@bibliotheca-dao/ui-lib/icons/topology.svg';
+import Yagi from '@bibliotheca-dao/ui-lib/icons/yagi.svg';
 export const PartnerBanner = () => {
   const partners = [
     {
@@ -11,13 +13,21 @@ export const PartnerBanner = () => {
       url: 'https://starkware.co/starknet/',
     },
     {
-      icon: <h2 className="text-orange-600">Briq</h2>,
+      icon: <Yagi className="w-32" />,
+      url: 'https://briq.construction',
+    },
+    {
+      icon: <span className="text-4xl text-orange-600">Briq</span>,
+      url: 'https://briq.construction',
+    },
+    {
+      icon: <Topology className="w-48" />,
       url: 'https://briq.construction',
     },
   ];
   return (
     <div className="relative z-20 flex flex-wrap justify-center w-full h-auto p-10 text-gray-900 shadow-inner sm:p-20 sm:space-x-10 bg-off-300/40 text-off-300">
-      <h4>We work with</h4>{' '}
+      <h4 className="absolute top-0 self-center -mt-4">We work with</h4>{' '}
       {partners.map((a, index) => {
         return (
           <div key={index} className="self-center p-4">
