ffb0b699e8d0882ebf7af089a6c7f1eb2aebdd89
Michael Nolivos <michaelnolivos@gmail.com>
2022-07-28 17:56:03 -0400

    feat: raiding client side validation #183

--- apps/atlas/src/components/panels/RealmDetails/Army.tsx
+++ apps/atlas/src/components/panels/RealmDetails/Army.tsx
@@ -1,5 +1,5 @@
 import useCountdown from '@bibliotheca-dao/core-lib/hooks/use-countdown';
-import { Button } from '@bibliotheca-dao/ui-lib/base';
+import { Button, OrderIcon } from '@bibliotheca-dao/ui-lib/base';
 import React, { useEffect, useState } from 'react';
 import AtlasSidebar from '@/components/sidebars/AtlasSideBar';
 import { RealmResources } from '@/components/tables/RealmResources';
@@ -57,6 +57,19 @@ const Army: React.FC<Prop> = (props) => {
   const troops =
     realm.troops?.filter((squad) => squad.squadSlot === Squad[squadSlot]) ?? [];
 
+  const attackingRealm =
+    selectedRealms.length > 0 ? selectedRealms[0] : undefined;
+
+  // Client side validation
+  const isSameOrder = realm.orderType == attackingRealm?.orderType;
+  const attackRealmHasAttackSquad =
+    attackingRealm?.troops &&
+    attackingRealm.troops.filter((t) => t.squadSlot == Squad['Attack']).length >
+      0;
+
+  const raidButtonEnabled =
+    !!attackingRealm && !isSameOrder && attackRealmHasAttackSquad;
+
   return (
     <>
       <div className="px-2 py-1 font-semibold tracking-widest bg-gray-800">
@@ -81,22 +94,39 @@ const Army: React.FC<Prop> = (props) => {
       />
       <AtlasSidebar isOpen={isRaiding}>
         <SidebarHeader
-          title="Battle Plan"
+          title="Raiding Plan"
           onClose={() => setIsRaiding(false)}
         ></SidebarHeader>
         <h3>Attacking Realm</h3>
         <RealmSelector onSelect={(r) => setSelectedRealms(r)} />
         <div className="flex justify-between">
-          <h3>Defenders</h3>
-          <h3>Attacker</h3>
+          <h3>
+            Defender{' '}
+            <OrderIcon
+              withTooltip
+              className="inline-block"
+              size="sm"
+              order={realm.orderType}
+            />
+          </h3>
+          <h3>
+            {attackingRealm && (
+              <OrderIcon
+                withTooltip
+                size="sm"
+                order={attackingRealm?.orderType}
+              />
+            )}{' '}
+            Attacker
+          </h3>
         </div>
         <div className="grid grid-cols-2 gap-2 divide-x-2 divide-dotted divide-slate-200">
           <SquadStatistics troops={realm.troops || []}></SquadStatistics>
-          {selectedRealms && selectedRealms.length > 0 ? (
+          {attackingRealm ? (
             <SquadStatistics
               className="pl-2"
               reversed
-              troops={selectedRealms[0]?.troops || []}
+              troops={attackingRealm?.troops || []}
             ></SquadStatistics>
           ) : (
             <div className="flex items-center justify-center">
@@ -113,11 +143,27 @@ const Army: React.FC<Prop> = (props) => {
               defendingRealmId: realm.realmId,
             });
           }}
+          disabled={!raidButtonEnabled}
           variant="primary"
           className="w-full mt-2"
         >
-          Order the Assault
+          Start Raid
         </Button>
+        {!raidButtonEnabled && attackingRealm && (
+          <div className="p-2 my-2 text-orange-800 bg-red-200 rounded">
+            {isSameOrder && (
+              <p>
+                Ser, {attackingRealm.name} cannot Attack a Realm of the same
+                order!
+              </p>
+            )}
+            {!attackRealmHasAttackSquad && (
+              <p>
+                Ser, {attackingRealm.name} has not trained an Attacking Army!
+              </p>
+            )}
+          </div>
+        )}
       </AtlasSidebar>
       <div className="col-span-6 md:col-start-3 md:col-end-5">
         <RealmResources
