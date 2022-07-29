4a229a71a90030914b0e31ac23ebf19221269d1c
Michael Nolivos <michaelnolivos@gmail.com>
2022-07-29 10:26:50 -0400

    style: shorter banner heading

--- apps/atlas/src/shared/RealmBannerHeading.tsx
+++ apps/atlas/src/shared/RealmBannerHeading.tsx
@@ -32,7 +32,7 @@ export const RealmBannerHeading = (props: HeaderProps) => {
 
   return (
     <div
-      className={`bg-opacity-90 flex p-4 flex-wrap shadow bg-order-${props.order
+      className={`bg-opacity-90 flex p-2 flex-wrap shadow bg-order-${props.order
         .replace('the ', '')
         .replace('the_', '')} text-order-secondary-${props.order
         .replace('the ', '')
@@ -40,7 +40,7 @@ export const RealmBannerHeading = (props: HeaderProps) => {
     >
       <div className="flex justify-between w-full text-2xl tracking-widest text-center uppercase ">
         <div
-          className={`absolute ml-12 md:ml-4 z-10 flex justify-center w-10 h-20 md:w-12 md:h-24 -mt-4 bg-white border-4 border-order-${props.order
+          className={`absolute ml-12 md:ml-4 z-10 flex justify-center w-10 h-16 md:w-12 md:h-20 -mt-4 bg-white border-4 border-order-${props.order
             .replace('the ', '')
             .replace('the_', '')} border-double rounded-b-full shadow-xl`}
         >
@@ -53,7 +53,7 @@ export const RealmBannerHeading = (props: HeaderProps) => {
           />
         </div>
         <div className="self-center mx-auto">
-          <p className="text-2xl font-lords md:text-6xl">
+          <p className="text-2xl font-lords md:text-5xl">
             {props.title || 'Loading'}
           </p>
         </div>
