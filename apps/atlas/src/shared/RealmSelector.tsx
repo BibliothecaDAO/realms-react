c02c4414756879c2397fef255537bd4164e0aefd
Michael Nolivos <michaelnolivos@gmail.com>
2022-07-28 23:00:55 -0400

    feat: realm selector in sidebar #192

--- apps/atlas/src/shared/RealmSelector.tsx
+++ apps/atlas/src/shared/RealmSelector.tsx
@@ -1,5 +1,9 @@
+import { Button, OrderIcon } from '@bibliotheca-dao/ui-lib/base';
+import { useState } from 'react';
+import AtlasSidebar from '@/components/sidebars/AtlasSideBar';
 import type { GetRealmsQuery } from '@/generated/graphql';
 import useRealms from '@/hooks/settling/useRealms';
+import SidebarHeader from './SidebarHeader';
 
 type Props = {
   onSelect: (selected: GetRealmsQuery['realms']) => void;
@@ -7,19 +11,43 @@ type Props = {
 
 const RealmSelector = (props: Props) => {
   const realms = useRealms();
+  const [selectedRealms, setSelectedRealms] = useState<
+    GetRealmsQuery['realms']
+  >([]);
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="w-full">
-      {realms.data &&
-        !realms.loading &&
-        realms.data.realms.map((r) => (
-          <button
-            onClick={() => props.onSelect([r])}
-            className="w-full border hover:bg-gray-500 p-4"
-            key={r.realmId}
-          >
-            {r.name}
-          </button>
-        ))}
+      <Button
+        onClick={() => setSidebarOpen(true)}
+        className="w-full my-2"
+        variant="secondary"
+      >
+        {selectedRealms.length == 0 ? 'Select Realm' : selectedRealms[0].name}
+      </Button>
+
+      <AtlasSidebar isOpen={sidebarOpen}>
+        <SidebarHeader
+          title="Select Realm"
+          onClose={() => setSidebarOpen(false)}
+        ></SidebarHeader>
+        {realms.data &&
+          !realms.loading &&
+          realms.data.realms.map((r) => (
+            <button
+              onClick={() => {
+                setSidebarOpen(false);
+                setSelectedRealms([r]);
+                props.onSelect([r]);
+              }}
+              className="w-full p-4 border hover:bg-gray-500"
+              key={r.realmId}
+            >
+              <OrderIcon size="sm" order={r.orderType} /> {r.name}
+            </button>
+          ))}
+      </AtlasSidebar>
     </div>
   );
 };
