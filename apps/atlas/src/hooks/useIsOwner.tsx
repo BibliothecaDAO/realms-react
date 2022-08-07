99bc676a4436fabd8f594d92f6fd1f32528d0dfa
RedBeard <redbeard.eth@protonmail.com>
2022-08-08 08:04:43 +1000

    feat: add raid button back

--- apps/atlas/src/hooks/useIsOwner.tsx
+++ apps/atlas/src/hooks/useIsOwner.tsx
@@ -4,7 +4,8 @@ import { BigNumber } from 'ethers';
 const useIsOwner = (owner?: string | null) => {
   const { account } = useStarknet();
   const starknetWallet = account ? BigNumber.from(account).toHexString() : '';
-
+  console.log(owner);
+  console.log(account);
   if (account) {
     return starknetWallet == owner ? true : false;
   } else {
