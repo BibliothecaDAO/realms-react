7c4c349b3563d03a6d1bf19324c65ad16db3ed23
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-08-16 12:14:28 +1000

    feat: add in actual claimable

--- apps/bibliotheca-dao/src/pages/claim.tsx
+++ apps/bibliotheca-dao/src/pages/claim.tsx
@@ -3,9 +3,10 @@
 import { Button } from '@bibliotheca-dao/ui-lib/base';
 import { ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
-import Web3Utils from 'web3-utils';
+import Web3Utils, { toBN } from 'web3-utils';
 import PaymentPool from '@/abi/PaymentPool.json';
 import { MainLayout } from '@/components/layout/MainLayout';
+import UserClaim from '@/data/claim.json';
 import { useWalletContext } from '@/hooks/useWalletContext';
 import CumulativePaymentTree from '@/util/cumulative-payment-tree';
 
@@ -24,6 +25,36 @@ function Claim() {
   const [claimAmount, setClaimAmount] = useState('200000000000000000000000');
   const { signer, account } = useWalletContext();
 
+  const week = 1;
+
+  const amountPerWeekCalc = (amount: any) => {
+    const pre = amount * (week / 10);
+
+    const toEth = Web3Utils.toWei(pre.toString());
+
+    return Web3Utils.toBN(toEth);
+  };
+
+  const formattedClaim = UserClaim.map((a: any) => {
+    return {
+      payee: a.payee,
+      amount: amountPerWeekCalc(a.amount),
+    };
+  });
+
+  useEffect(() => {
+    const fetchBalances: any =
+      formattedClaim.find((a: any) => a.payee === account.toLowerCase()) ||
+      toBN(0);
+
+    if (fetchBalances.amount) {
+      const toEth = ethers.utils.formatEther(
+        fetchBalances.amount.toString() || 0
+      );
+      setClaimAmount(toEth);
+    }
+  });
+
   async function claim() {
     const paymentPool = new ethers.Contract(
       PaymentPool.address,
@@ -31,14 +62,15 @@ function Claim() {
       signer
     );
 
-    const paymentTree = new CumulativePaymentTree(paymentList);
+    const paymentTree = new CumulativePaymentTree(formattedClaim);
 
-    const proof = paymentTree.hexProofForPayee(account, 3);
+    const proof = paymentTree.hexProofForPayee(account.toLowerCase(), week);
+
+    const formatAmount = Web3Utils.toWei(claimAmount);
 
     console.log(proof);
-
     try {
-      await paymentPool.withdraw(claimAmount, proof);
+      await paymentPool.withdraw(formatAmount, proof);
     } catch (e) {
       console.log(e);
     }
@@ -48,7 +80,17 @@ function Claim() {
     <MainLayout>
       <div className="container px-10 py-20 pt-40 mx-auto sm:px-20 sm:py-40 sm:pt-60">
         <h1 className="mb-8">Claim Airdrop</h1>
-        <p>Enter your claim amount. You do not have to claim everyweek.</p>
+
+        <div className="mb-4">
+          <h4>Your total claimable</h4>
+          <h2>
+            {(parseInt(claimAmount) * (10 / week)).toLocaleString()} $LORDS
+          </h2>
+        </div>
+        <p>
+          Your total claimable unlocks 10% per week. You do not need to claim
+          every week.{' '}
+        </p>
         <div>
           <div className="flex flex-wrap w-72">
             <input
