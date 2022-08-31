0673823d8d35e569b83add1be3715a10bee49e99
0x1337 <deadw00d@mail.ru>
2022-08-31 15:04:00 +0700

    feat: added tooltip component wrapper

---
+++ packages/ui-lib/src/base/utility/tooltip.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react';
+
+export const Tooltip = ({
+  children,
+  tooltipText,
+}: {
+  children: ReactElement;
+  tooltipText: string;
+}) => {
+  return (
+    <div className="relative flex flex-col items-center justify-center group">
+      {children}
+      <div className="absolute top-0 -translate-y-full w-max flex flex-col items-center hidden group-hover:flex">
+        <span className="relative rounded z-100 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">
+          {tooltipText}
+        </span>
+        <div className="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
+      </div>
+    </div>
+  );
+};
+
+Tooltip.displayName = 'Tooltip';
