4b50795246614e4656814368d3eefceb347b8700
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-09-17 20:44:02 +1000

    feat: contin work on army builder

--- apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
+++ apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
@@ -1,60 +1,87 @@
-import { Card } from '@bibliotheca-dao/ui-lib/base';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+} from '@bibliotheca-dao/ui-lib/base';
+import RadarMap from '@bibliotheca-dao/ui-lib/graph/Radar';
 import React, { useState } from 'react';
 import type { Army } from '@/generated/graphql';
 import { useArmy } from '@/hooks/settling/useArmy';
 import SquadStatistics from '@/shared/squad/SquadStatistics';
+import type { ArmyInterface } from '@/types/index';
 
 type Prop = {
   army?: Army;
 };
 
 type Battalion = {
-  id: number;
-  name: string;
-  quantity: string;
-  health: string;
+  battalionId: number;
+  battalionName: string;
 };
 
-export const Battalion: React.FC<Battalion> = (props) => {
+const blankB = { battalionId: 1, battalionName: 'sss' };
+
+export const Battalion: React.FC<
+  ArmyInterface & { add: (id) => void; quantity; health }
+> = (props) => {
   return (
-    <Card key={props.id} className="flex-col ">
-      <h3 className="uppercase">{props.name}</h3>
-      <div>Battlions: {props.quantity}</div>
-      <div>Battlion Health: {props.health}</div>
+    <Card key={props.battalionId} className="relative flex-col group">
+      <div className="absolute flex justify-center invisible w-full h-full -m-3 transition-all bg-black cursor-pointer rounded-3xl opacity-90 group-hover:visible">
+        <Button
+          onClick={() =>
+            props.add({
+              battalionId: props.battalionId,
+              battalionName: props.battalionName,
+            })
+          }
+          variant="outline"
+          className="self-center"
+        >
+          add to army +
+        </Button>
+      </div>
+      <CardTitle>{props.battalionName}</CardTitle>
+      <CardBody>
+        <div>Battlions: {props.quantity}</div>
+        <div>Battlion Health: {props.health}</div>
+      </CardBody>
     </Card>
   );
 };
 
-const blankB = { id: 1, name: 'sss', quantity: '2', health: '100' };
-
 export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
   const [activeBattalion, setActiveBattalion] = useState<Battalion>(blankB);
+  const [addedBattalions, setAddedBattalions] = useState<Battalion[]>([]);
 
   const army = props.army;
   const { armyBattalions } = useArmy();
 
   return (
-    <div className="flex space-x-10">
-      <div className="w-2/3">
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-7">
         <h2 className="mt-4">Realm {army?.realmId}</h2>
-        <h2>Current Army #{army?.armyId}</h2>
+        <h4>
+          Current Army #{army?.armyId} | Location:{' '}
+          {army?.visitingRealmId != 0 ? army?.visitingRealmId : 'Home'}
+        </h4>
 
         <div className="grid w-full grid-cols-2 gap-4">
           {armyBattalions?.map((battalion, index) => (
             <div
               onMouseEnter={() =>
                 setActiveBattalion({
-                  id: battalion.battalionId,
-                  name: battalion.battalionName,
-                  quantity: '2',
-                  health: '100',
+                  battalionId: battalion.battalionId,
+                  battalionName: battalion.battalionName,
                 })
               }
               key={index}
             >
               <Battalion
-                id={battalion.battalionId}
-                name={battalion.battalionName}
+                {...battalion}
+                add={(value) =>
+                  setAddedBattalions((current) => [...current, value])
+                }
                 quantity={army ? army[battalion.battalionName + 'Qty'] : ''}
                 health={army ? army[battalion.battalionName + 'Health'] : ''}
               />
@@ -62,20 +89,47 @@ export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
           ))}
         </div>
       </div>
-      <div className="w-1/3">
-        <h3 className="">
-          Location:{' '}
-          {army?.visitingRealmId != 0 ? army?.visitingRealmId : 'Home'}
-        </h3>
-        <h2>Recruitment Costs</h2>
-        <div className="p-4 border rounded">
-          <h2>{activeBattalion.name}</h2>
-          <p>
-            Description Description Description DescriptionDescriptionDe
-            scriptionDescriptionDescriptionDescri ptionDescriptionDescription
-            DescriptionDescriptionD scriptionDescriptionD escription
-          </p>
-        </div>
+      <div className="col-span-5">
+        <h2>Battalion Details</h2>
+        <Card className="">
+          <CardTitle>{activeBattalion.battalionName}</CardTitle>
+          <CardBody>
+            <p>
+              Paladins are the most loyal and talented horseback riders that
+              exist. With their superior horsemanship skills, they are able to
+              wield a lance or polearm effectively with just one hand, allowing
+              them to use their other hand to carry a shield or sheathe a
+              weapon. As well as being proficient with most weapons and
+              protective armor, Paladins are also highly skilled at defensive
+              maneuvers while mounted.
+            </p>
+          </CardBody>
+        </Card>
+      </div>
+      <Card className="col-span-7">
+        <CardTitle>Adding Battalions to build</CardTitle>
+        <CardBody>
+          <div className="grid w-full grid-cols-2 gap-4">
+            {addedBattalions?.map((battalion, index) => (
+              <div key={index}>{battalion.battalionId}</div>
+              // <Battalion
+              //   {...battalion}
+              //   key={index}
+              //   add={(value) => setAddedBattalions(value)}
+              //   quantity={army ? army[battalion.battalionName + 'Qty'] : ''}
+              //   health={army ? army[battalion.battalionName + 'Health'] : ''}
+              // />
+            ))}
+          </div>
+        </CardBody>
+      </Card>
+      <div className="col-span-5">
+        <RadarMap height={400} width={400} />
+      </div>
+      <div className="col-span-5">
+        <Button variant="primary" size="lg">
+          muster the battalions
+        </Button>
       </div>
     </div>
   );
---
+++ packages/ui-lib/src/graph/Radar.tsx
@@ -0,0 +1,134 @@
+import { Group } from '@visx/group';
+import type { LetterFrequency } from '@visx/mock-data/lib/mocks/letterFrequency';
+import letterFrequency from '@visx/mock-data/lib/mocks/letterFrequency';
+import { Point } from '@visx/point';
+import { scaleLinear } from '@visx/scale';
+import { Line, LineRadial } from '@visx/shape';
+import React from 'react';
+
+const orange = '#ff9933';
+export const pumpkin = '#f5810c';
+const silver = '#d9d9d9';
+export const background = '#FAF7E9';
+
+const degrees = 360;
+const data = letterFrequency.slice(2, 12);
+
+const y = (d: LetterFrequency) => d.frequency;
+
+const genAngles = (length: number) =>
+  [...new Array(length + 1)].map((_, i) => ({
+    angle: i * (degrees / length),
+  }));
+
+const genPoints = (length: number, radius: number) => {
+  const step = (Math.PI * 2) / length;
+  return [...new Array(length)].map((_, i) => ({
+    x: radius * Math.sin(i * step),
+    y: radius * Math.cos(i * step),
+  }));
+};
+
+function genPolygonPoints<Datum>(
+  dataArray: Datum[],
+  scale: (n: number) => number,
+  getValue: (d: Datum) => number
+) {
+  const step = (Math.PI * 2) / dataArray.length;
+  const points: { x: number; y: number }[] = new Array(dataArray.length).fill({
+    x: 0,
+    y: 0,
+  });
+  const pointString: string = new Array(dataArray.length + 1)
+    .fill('')
+    .reduce((res, _, i) => {
+      if (i > dataArray.length) return res;
+      const xVal = scale(getValue(dataArray[i - 1])) * Math.sin(i * step);
+      const yVal = scale(getValue(dataArray[i - 1])) * Math.cos(i * step);
+      points[i - 1] = { x: xVal, y: yVal };
+      res += `${xVal},${yVal} `;
+      return res;
+    });
+
+  return { points, pointString };
+}
+
+const defaultMargin = { top: 40, left: 40, right: 40, bottom: 40 };
+
+export type RadarProps = {
+  width: number;
+  height: number;
+  margin?: { top: number; right: number; bottom: number; left: number };
+  levels?: number;
+};
+
+export default function RadarMap({
+  width,
+  height,
+  levels = 5,
+  margin = defaultMargin,
+}: RadarProps) {
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
+  const radius = Math.min(xMax, yMax) / 2;
+
+  const radialScale = scaleLinear<number>({
+    range: [0, Math.PI * 2],
+    domain: [degrees, 0],
+  });
+
+  const yScale = scaleLinear<number>({
+    range: [0, radius],
+    domain: [0, Math.max(...data.map(y))],
+  });
+
+  const webs = genAngles(data.length);
+  const points = genPoints(data.length, radius);
+  const polygonPoints = genPolygonPoints(data, (d) => yScale(d) ?? 0, y);
+  const zeroPoint = new Point({ x: 0, y: 0 });
+
+  return width < 10 ? null : (
+    <svg width={width} height={height}>
+      <rect fill={background} width={width} height={height} rx={14} />
+      <Group top={height / 2 - margin.top} left={width / 2}>
+        {[...new Array(levels)].map((_, i) => (
+          <LineRadial
+            key={`web-${i}`}
+            data={webs}
+            angle={(d) => radialScale(d.angle) ?? 0}
+            radius={((i + 1) * radius) / levels}
+            fill="none"
+            stroke={silver}
+            strokeWidth={2}
+            strokeOpacity={0.8}
+            strokeLinecap="round"
+          />
+        ))}
+        {[...new Array(data.length)].map((_, i) => (
+          <Line
+            key={`radar-line-${i}`}
+            from={zeroPoint}
+            to={points[i]}
+            stroke={silver}
+          />
+        ))}
+        <polygon
+          points={polygonPoints.pointString}
+          fill={orange}
+          fillOpacity={0.3}
+          stroke={orange}
+          strokeWidth={1}
+        />
+        {polygonPoints.points.map((point, i) => (
+          <circle
+            key={`radar-point-${i}`}
+            cx={point.x}
+            cy={point.y}
+            r={3}
+            fill={pumpkin}
+          />
+        ))}
+      </Group>
+    </svg>
+  );
+}
