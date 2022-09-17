5cb160a242194c7c81d5b5bb15f353f7725733fd
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-09-17 11:37:59 +1000

    feat: army builder

--- apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
+++ apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Card } from '@bibliotheca-dao/ui-lib/base';
+import React, { useState } from 'react';
 import { troopList } from '@/constants/troops';
 import type { Army } from '@/generated/graphql';
 
@@ -8,34 +9,74 @@ type Prop = {
   army?: Army;
 };
 
+type Battalion = {
+  id: number;
+  name: string;
+  quantity: string;
+  health: string;
+  onMouseEnter?: () => void;
+};
+
+export const Battalion: React.FC<Battalion> = (props) => {
+  return (
+    <Card
+      onMouseEnter={props.onMouseEnter}
+      key={props.id}
+      className="flex-col "
+    >
+      <h3 className="uppercase">{props.name}</h3>
+      <div>Battlions: {props.quantity}</div>
+      <div>Battlion Health: {props.health}</div>
+    </Card>
+  );
+};
+
+const blankB = { id: 1, name: 'sss', quantity: '2', health: '100' };
+
 export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
+  const [activeBattalion, setActiveBattalion] = useState<Battalion>(blankB);
+
   const army = props.army;
 
   return (
-    <div className="flex">
+    <div className="flex space-x-10">
       <div className="w-2/3">
         <h2 className="mt-4">Realm {army?.realmId}</h2>
         <h2>Current Army #{army?.armyId}</h2>
-        <div className="grid w-full grid-cols-4 gap-4">
-          {troopList.map((troop) => (
-            <div key={troop.troopId} className="flex-col p-4 border">
-              {troop.name}
-              {army && (
-                <div>
-                  Qty: {army[troop.name + 'Qty']}
-                  Health: {army[troop.name + 'Health']}
-                </div>
-              )}
-            </div>
+        <div className="grid w-full grid-cols-2 gap-4">
+          {troopList.map((troop, index) => (
+            <Battalion
+              onMouseEnter={() =>
+                setActiveBattalion({
+                  id: troop.troopId,
+                  name: troop.name,
+                  quantity: '2',
+                  health: '100',
+                })
+              }
+              key={index}
+              id={troop.troopId}
+              name={troop.name}
+              quantity={army ? army[troop.name + 'Qty'] : ''}
+              health={army ? army[troop.name + 'Health'] : ''}
+            />
           ))}
         </div>
       </div>
-      <div className="w-1/3 -mt-12">
+      <div className="w-1/3">
         <h3 className="">
           Location:{' '}
           {army?.visitingRealmId != 0 ? army?.visitingRealmId : 'Home'}
         </h3>
         <h2>Recruitment Costs</h2>
+        <div className="p-4 border rounded">
+          <h2>{activeBattalion.name}</h2>
+          <p>
+            Description Description Description DescriptionDescriptionDe
+            scriptionDescriptionDescriptionDescri ptionDescriptionDescription
+            DescriptionDescriptionD scriptionDescriptionD escription
+          </p>
+        </div>
       </div>
     </div>
   );
