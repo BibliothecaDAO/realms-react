12ca8471d7d5bf331a46f03d3f9aecbe1fe92087
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2022-09-18 15:13:43 +1000

    feat: build battalion UI

--- apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
+++ apps/atlas/src/components/sidebars/ArmyBuilderSideBar.tsx
@@ -3,13 +3,16 @@ import {
   Card,
   CardBody,
   CardTitle,
+  InputNumber,
 } from '@bibliotheca-dao/ui-lib/base';
 import { RadarMap } from '@bibliotheca-dao/ui-lib/graph/Radar';
 import Image from 'next/image';
+import type { ValueType } from 'rc-input-number/lib/utils/MiniDecimal';
 import React, { useState } from 'react';
 import { battalionInformation } from '@/constants/army';
 import type { Army } from '@/generated/graphql';
 import { useArmy } from '@/hooks/settling/useArmy';
+import useCombat from '@/hooks/settling/useCombat';
 import SquadStatistics from '@/shared/squad/SquadStatistics';
 import type { ArmyInterface } from '@/types/index';
 
@@ -20,6 +23,7 @@ type Prop = {
 type Battalion = {
   battalionId: number;
   battalionName: string;
+  battalionQty: number;
 };
 
 const blankB = { battalionId: 1, battalionName: 'sss' };
@@ -28,31 +32,48 @@ export const Battalion: React.FC<
   ArmyInterface & { add: (id) => void; quantity; health }
 > = (props) => {
   const data = battalionInformation.find((a) => a.id === props.battalionId);
+
+  const [input, setInput] = useState('1');
   return (
     <Card
       key={props.battalionId}
       className={`relative flex-col group ${data?.color}`}
     >
-      {/* <div className="absolute flex justify-center invisible w-full h-full -m-3 transition-all bg-black cursor-pointer rounded-3xl opacity-90 group-hover:visible">
-        <Button
-          onClick={() =>
-            props.add({
-              battalionId: props.battalionId,
-              battalionName: props.battalionName,
-            })
-          }
-          variant="outline"
-          className="self-center"
-        >
-          add to army +
-        </Button>
-      </div> */}
+      <div className="absolute flex justify-center invisible w-full h-full -m-3 transition-all bg-black cursor-pointer rounded-3xl opacity-90 group-hover:visible">
+        <div className="flex self-center">
+          <Button
+            onClick={() =>
+              props.add({
+                battalionId: props.battalionId,
+                battalionName: props.battalionName,
+                battalionQty: input,
+              })
+            }
+            variant="primary"
+            size="xs"
+            className="self-center"
+          >
+            add to army +
+          </Button>
+          <InputNumber
+            value={input}
+            inputSize="md"
+            colorScheme="transparent"
+            className="self-center w-12 h-full bg-white border rounded border-white/40"
+            min={1}
+            max={23}
+            stringMode // to support high precision decimals
+            onChange={(value: any) => setInput(value)}
+          />{' '}
+        </div>
+      </div>
       <CardTitle>{props.battalionName}</CardTitle>
       <CardBody>
         <div>Battlions: {props.quantity}</div>
         <div>Battlion Health: {props.health}</div>
         <div>
-          <h5>Strong vs Archers</h5>
+          <h5>Strong vs {data?.strength}</h5>
+          <h5>Weak vs {data?.weakness}</h5>
         </div>
       </CardBody>
     </Card>
@@ -60,11 +81,23 @@ export const Battalion: React.FC<
 };
 
 export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
-  const [activeBattalion, setActiveBattalion] = useState<Battalion>(blankB);
+  const { build } = useCombat();
+  const [activeBattalion, setActiveBattalion] = useState<Battalion>();
   const [addedBattalions, setAddedBattalions] = useState<Battalion[]>([]);
 
   const army = props.army;
   const { armyBattalions } = useArmy();
+
+  const activeBattalionData = battalionInformation.find(
+    (a) => a.id === activeBattalion?.battalionId
+  );
+
+  const removeFromArray = (index) => {
+    setAddedBattalions([
+      ...addedBattalions.slice(0, index),
+      ...addedBattalions.slice(index + 1),
+    ]);
+  };
 
   return (
     <div className="grid grid-cols-12 gap-6">
@@ -82,6 +115,7 @@ export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
                 setActiveBattalion({
                   battalionId: battalion.battalionId,
                   battalionName: battalion.battalionName,
+                  battalionQty: 1,
                 })
               }
               key={index}
@@ -99,25 +133,20 @@ export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
         </div>
       </div>
       <div className="col-span-5">
-        <Card className="">
+        <Card
+          className={`shadow-white shadow-lg ${activeBattalionData?.color}`}
+        >
           <Image
-            className="rounded-xl"
-            src={'/stableai/paladin.jpeg'}
+            className="rounded-xl "
+            src={activeBattalionData?.image ? activeBattalionData.image : ''}
             width={400}
             height={400}
           />
-          <CardTitle>{activeBattalion.battalionName}</CardTitle>
-          <CardBody>
-            <p>
-              Paladins are the most loyal and talented horseback riders that
-              exist. With their superior horsemanship skills, they are able to
-              wield a lance or polearm effectively with just one hand, allowing
-              them to use their other hand to carry a shield or sheathe a
-              weapon. As well as being proficient with most weapons and
-              protective armor, Paladins are also highly skilled at defensive
-              maneuvers while mounted.
-            </p>
-          </CardBody>
+          <div className="p-3">
+            <h2>{activeBattalion?.battalionName}</h2>
+          </div>
+
+          <CardBody>{activeBattalionData?.description}</CardBody>
         </Card>
       </div>
       <Card className="col-span-7">
@@ -125,14 +154,18 @@ export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
         <CardBody>
           <div className="grid w-full grid-cols-2 gap-4">
             {addedBattalions?.map((battalion, index) => (
-              <div key={index}>{battalion.battalionId}</div>
-              // <Battalion
-              //   {...battalion}
-              //   key={index}
-              //   add={(value) => setAddedBattalions(value)}
-              //   quantity={army ? army[battalion.battalionName + 'Qty'] : ''}
-              //   health={army ? army[battalion.battalionName + 'Health'] : ''}
-              // />
+              <Card className="" key={index}>
+                <CardTitle>{battalion.battalionName} </CardTitle>
+                <CardBody>Battalions: {battalion.battalionQty}</CardBody>
+
+                <Button
+                  size="xs"
+                  variant="primary"
+                  onClick={() => removeFromArray(index)}
+                >
+                  remove
+                </Button>
+              </Card>
             ))}
           </div>
         </CardBody>
@@ -141,7 +174,19 @@ export const ArmyBuilderSideBar: React.FC<Prop> = (props) => {
         <RadarMap width={400} height={400} />
       </div>
       <div className="col-span-5">
-        <Button variant="primary" size="lg">
+        <Button
+          onClick={() => {
+            build(
+              army?.realmId,
+              army?.armyId,
+              addedBattalions.map((a) => a.battalionId),
+              addedBattalions.map((a) => a.battalionQty)
+            );
+            setAddedBattalions(() => []);
+          }}
+          variant="primary"
+          size="lg"
+        >
           muster the battalions
         </Button>
       </div>
--- apps/atlas/src/constants/army.ts
+++ apps/atlas/src/constants/army.ts
@@ -8,10 +8,10 @@ export enum TroopSlot {
   defending = 2,
 }
 
-const Barracks = 'bg-red-600';
-const ArcherTower = 'bg-yellow-600';
-const Castle = 'bg-orange-600';
-const MageTower = 'bg-purple-600';
+const Barracks = 'bg-red-600/20';
+const ArcherTower = 'bg-yellow-600/20';
+const Castle = 'bg-orange-600/20';
+const MageTower = 'bg-purple-600/20';
 
 const image = '/stableai/paladin.jpeg';
 
@@ -19,50 +19,66 @@ export const battalionInformation = [
   {
     id: 1,
     color: Barracks,
-    image: image,
-    description: '1',
-    strength: '',
+    image: '/stableai/knight.jpeg',
+    description:
+      'Paladins are the most loyal and talented horseback riders that exist. With their superior horsemanship skills, they are able to wield a lance or polearm effectively with just one hand, allowing them to use their other hand to carry a shield or sheathe a weapon. As well as being proficient with most weapons and protective armor, Paladins are also highly skilled at defensive maneuvers while mounted.',
+    strength: 'Archers',
+    weakness: 'Infantry',
   },
   {
     id: 2,
     color: Barracks,
     image: image,
     description: '2',
+    strength: 'Archers',
+    weakness: 'Infantry',
   },
   {
     id: 3,
     color: ArcherTower,
     image: image,
     description: '3',
+    strength: 'Magic',
+    weakness: 'Calvary',
   },
   {
     id: 4,
     color: ArcherTower,
     image: image,
     description: '4',
+    strength: 'Magic',
+    weakness: 'Calvary',
   },
   {
     id: 5,
     color: Castle,
     image: image,
     description: '5',
+    strength: 'Infantry',
+    weakness: 'Archers',
   },
   {
     id: 6,
     color: Castle,
     image: image,
     description: '6',
+    strength: 'Infantry',
+    weakness: 'Archers',
   },
   {
     id: 7,
     color: MageTower,
-    image: image,
+    image: '/stableai/mage.jpeg',
     description: '7',
+    strength: 'Calvary',
+    weakness: 'Magic',
   },
   {
     id: 8,
     color: MageTower,
-    image: image,
+    image: '/stableai/apprentice.jpeg',
     description: '8',
+    strength: 'Calvary',
+    weakness: 'Magic',
   },
 ];
