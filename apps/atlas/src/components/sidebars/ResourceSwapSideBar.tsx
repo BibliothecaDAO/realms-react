a10eb74f358c7eb0734f60f2179cb1a8f3dae966
RedBeard <redbeard.eth@protonmail.com>
2022-10-23 13:43:04 +1100

    fix: z-index on bank swap

--- apps/atlas/src/components/sidebars/ResourceSwapSideBar.tsx
+++ apps/atlas/src/components/sidebars/ResourceSwapSideBar.tsx
@@ -23,7 +23,7 @@ export const ResourceSwapSideBar = ({
   resources,
 }: ResourceSwapSideBarProps) => {
   return (
-    <AtlasSideBar isOpen={isOpen} containerClassName="w-full lg:w-5/12 z-30">
+    <AtlasSideBar isOpen={isOpen} containerClassName="w-full lg:w-5/12 z-20">
       {isOpen && (
         <ResourceSwapSideBarPanel resources={resources} onClose={onClose} />
       )}
