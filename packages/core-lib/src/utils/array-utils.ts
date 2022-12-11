56e9bbe940b0a0c8897c530d8b8a20b7a1381c65
RedBeard <redbeard.eth@protonmail.com>
2022-12-12 09:22:19 +1100

    Revert "feat: remove core utils"
    This reverts commit f0210a1b81f42ad79a7231ba83d2c12bc92d360f.

---
+++ packages/core-lib/src/utils/array-utils.ts
@@ -0,0 +1,23 @@
+import { RandomUtils } from './random-utils';
+
+export class ArrayUtils {
+  static getRandom<T>(items: T[]): T {
+    return items[RandomUtils.getRandomInt(0, items.length - 1)];
+  }
+
+  static removeItem<T>(arr: T[], item: T): T[] {
+    const index = arr.indexOf(item);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+  /**
+   * Tests if this given value is even.
+   * @returns {boolean}
+   */
+  static isEven(value: number) {
+    return (value & 1) === 0;
+  }
+}
