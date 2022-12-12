150766d4c5dd2b315db613529939c3a278c1f595
RedBeard <redbeard.eth@protonmail.com>
2022-12-12 11:31:24 +1100

    fix: used imports

--- packages/core-lib/src/index.ts
+++ packages/core-lib/src/index.ts
@@ -1,10 +1,5 @@
-export { Asserts } from './utils/asserts';
 export { ArrayUtils } from './utils/array-utils';
 export { RandomUtils } from './utils/random-utils';
-export { isValidElement, cloneElement } from './utils/react-node';
-
-export * from './utils/typeguards';
-export type { UnPromisify } from './utils/type-utils';
 
 export const sayHello = (name: string): string => {
   return `I'm the @bibliotheca-dao/ui-lib component telling ${name} !`;
--- packages/core-lib/src/utils/type-utils.ts
+++
@@ -1,1 +0,0 @@
-export type UnPromisify<T> = T extends Promise<infer U> ? U : T;
