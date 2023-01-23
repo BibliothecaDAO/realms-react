b763237b59fdec34b6f54d6e30253a40785a162a
0x1337 <deadw00d@mail.ru>
2023-01-24 02:22:01 +0700

    fix: chat user messages detection improve

--- apps/atlas/src/components/web3mq/CustomMessageContainer.tsx
+++ apps/atlas/src/components/web3mq/CustomMessageContainer.tsx
@@ -1,5 +1,7 @@
 import { useAccount } from '@starknet-react/core';
 import clsx from 'clsx';
+import { useMemo } from 'react';
+import { useAccount as useL1Account } from 'wagmi';
 import { Avatar } from 'web3-mq-react';
 import type { MessageItem } from 'web3-mq-react';
 
@@ -10,9 +12,17 @@ export const CustomMessageContainer = (props: { message: MessageItem }) => {
   const { content, date, timestamp, senderId, senderInfo = {} } = message;
   const { avatar_url: avatarUrl, nickname } = senderInfo?.web3mqInfo || {};
   const { address: currentUserAddress } = useAccount();
+  const { address: currentUserL1Address } = useL1Account();
   const avatar = avatarUrl || senderInfo?.defaultUserAvatar;
   const nickName =
     nickname || senderInfo?.defaultUserName || getShortAddress(senderId);
+
+  const isCurrentUser = useMemo(
+    () =>
+      senderInfo?.wallet_address == currentUserAddress ||
+      senderInfo?.wallet_address == currentUserL1Address,
+    [senderInfo?.wallet_address, currentUserAddress, currentUserL1Address]
+  );
 
   if (senderId === botMainKeys.userid) {
     return (
@@ -29,31 +39,26 @@ export const CustomMessageContainer = (props: { message: MessageItem }) => {
     <div
       className={clsx(
         'flex items-end mb-4 mr-2',
-        senderInfo?.wallet_address == currentUserAddress && 'flex-row-reverse'
+        isCurrentUser && 'flex-row-reverse'
       )}
     >
       <Avatar
         name="user"
         image={avatar}
         size={30}
-        className={`${
-          senderInfo?.wallet_address !== currentUserAddress ? '' : 'ml-2 !mr-0'
-        }`}
+        className={`${!isCurrentUser ? '' : 'ml-2 !mr-0'}`}
       />
       <div
         className={`flex flex-col p-2 px-2 bg-white relative rounded-xl bg-opacity-5 w-fit ${
-          senderInfo?.wallet_address == currentUserAddress
-            ? 'ml-auto text-right'
-            : 'mr-auto'
+          isCurrentUser ? 'ml-auto text-right' : 'mr-auto'
         }`}
       >
         <div
           className="text-yellow-600"
           style={{
-            color:
-              senderInfo?.wallet_address == currentUserAddress
-                ? ''
-                : `#${senderInfo?.address.substr(2, 6) || '000000'}`,
+            color: isCurrentUser
+              ? ''
+              : `#${senderInfo?.address.substr(2, 6) || '000000'}`,
           }}
         >
           {nickName}
