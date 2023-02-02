976f5a3a53a4210cb53b310dd58dafa24baca15f
RedBeard <redbeard.eth@protonmail.com>
2023-02-02 17:01:16 +1100

    fix: wagmi chain connection

--- apps/atlas/src/util/wagmi.ts
+++ apps/atlas/src/util/wagmi.ts
@@ -1,14 +1,15 @@
 import { getDefaultClient } from 'connectkit';
+import type { Chain } from 'wagmi';
 import { configureChains, createClient } from 'wagmi';
 import { mainnet, goerli } from 'wagmi/chains';
 import { infuraProvider } from 'wagmi/providers/infura';
 import { publicProvider } from 'wagmi/providers/public';
 
-const supportedChains =
+const supportedChains: [Chain] =
   process.env.NEXT_PUBLIC_NETWORK === 'goerli' ? [goerli] : [mainnet];
 
 export const { chains, provider, webSocketProvider } = configureChains(
-  [goerli, mainnet],
+  supportedChains,
   [
     infuraProvider({
       apiKey: 'd55e01eb6d8e4003a472cb13e3fc3d77',
