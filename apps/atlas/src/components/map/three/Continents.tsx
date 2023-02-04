25d8539a885b7720cd2ef07aa18264bc3f099feb
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-02-04 12:28:07 +1100

    feat: geo features

--- apps/atlas/src/components/map/three/Continents.tsx
+++ apps/atlas/src/components/map/three/Continents.tsx
@@ -6,7 +6,9 @@ import { Fragment } from 'react';
 import * as THREE from 'three';
 import { GeoJsonGeometry } from 'three-geojson-geometry';
 
-const land = require('@/geodata/land.geojson');
+const coast = require('@/geodata/coastline.geojson');
+const land = require('@/geodata/landmass.geojson');
+const ocean = require('@/geodata/ocean.geojson');
 
 export default function Continents() {
   return (
@@ -14,6 +16,36 @@ export default function Continents() {
       {land ? (
         <Fragment>
           {land.features.map(({ geometry, properties }, index) => {
+            const g = new GeoJsonGeometry(geometry, 200);
+            return (
+              <lineSegments key={index} geometry={g}>
+                <meshToonMaterial
+                  attach="material"
+                  // color={properties.order_idx === 1 ? 'hotpink' : 'blue'}
+                />
+              </lineSegments>
+            );
+          })}
+        </Fragment>
+      ) : null}
+      {land ? (
+        <Fragment>
+          {coast.features.map(({ geometry, properties }, index) => {
+            const g = new GeoJsonGeometry(geometry, 200);
+            return (
+              <lineSegments key={index} geometry={g}>
+                <meshBasicMaterial
+                  attach="material"
+                  color={properties.order_idx === 1 ? 'hotpink' : 'blue'}
+                />
+              </lineSegments>
+            );
+          })}
+        </Fragment>
+      ) : null}
+      {land ? (
+        <Fragment>
+          {ocean.features.map(({ geometry, properties }, index) => {
             const g = new GeoJsonGeometry(geometry, 200);
             return (
               <lineSegments key={index} geometry={g}>
