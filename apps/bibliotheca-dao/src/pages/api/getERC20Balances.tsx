e8ab58af4770cbce7e371bed9e72866dfddfe76d
RedBeard <redbeard.eth@protonmail.com>
2023-03-15 11:47:56 +1100

    fix: remove logging

--- apps/bibliotheca-dao/src/pages/api/getERC20Balances.tsx
+++ apps/bibliotheca-dao/src/pages/api/getERC20Balances.tsx
@@ -22,7 +22,6 @@ export default async function handler(req: NextRequest) {
     const response = await fetch(url, {
       method: 'GET',
     });
-    console.log(response);
     return new Response(response.body, {
       status: response.status,
     });
