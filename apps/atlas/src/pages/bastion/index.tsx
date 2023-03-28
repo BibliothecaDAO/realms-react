2570011834c35cb942ccda0c53e79a7b7c689294
aymericdelab <aymericdelabrousse@gmail.com>
2023-03-28 10:50:43 +0200

    fix: show tx cart in Bastions

--- apps/atlas/src/pages/bastion/index.tsx
+++ apps/atlas/src/pages/bastion/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { BastionPanel } from '@/components/bastions/BastionPanel';
 import Layout from '@/components/Layout';
 import { ArtBackground } from '@/components/map/ArtBackground';
+import { TransactionCartSideBar } from '@/components/ui/transactions/TransactionCartSideBar';
 import { BastionProvider } from '@/context/BastionContext';
+import { useUIContext } from '@/context/UIContext';
 
 export default function BastionPage() {
+  const { transactionCart, toggleTransactionCart } = useUIContext();
   return (
     <Layout>
       <BastionProvider>
         <BastionPanel></BastionPanel>
       </BastionProvider>
+      <TransactionCartSideBar
+        isOpen={transactionCart}
+        onClose={toggleTransactionCart}
+      />
       <ArtBackground background="realm" />
     </Layout>
   );
